e6915709cd560bb6076bf88fd68e89de58eaac32
ecorte <antoine.turgeon2005@hotmail.com>
2021-06-10 20:11:35 -0400

    Update ready.ts

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -2,15 +2,17 @@
 import { Listener } from 'discord-akairo';
 
 export default class ReadyListener extends Listener {
-    constructor() {
-        super('ready', {
-            emitter: 'client',
-            event: 'ready'
-        });
-    }
+	constructor() {
+		super('ready', {
+			emitter: 'client',
+			event: 'ready'
+		});
+	}
 
-    exec() {
-      console.log(`Logged in as ${this.client.user.tag} | ${this.client.guilds.cache.size} servers`);
-      this.client.user.setActivity('yo allo ?', { type: 'WATCHING' })
-    }
+	exec() {
+		console.log(
+			`Logged in as ${this.client.user.tag} | ${this.client.guilds.cache.size} servers`
+		);
+		this.client.user.setActivity('yo allo ?', { type: 'WATCHING' });
+	}
 }
