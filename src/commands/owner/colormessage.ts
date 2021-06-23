8f51d04bef05f0bbd6e7f36a88ddf7a31deaf2a2
ecorte <antoine.turgeon2005@hotmail.com>
2021-06-23 18:10:10 -0400

    Update colormessage.ts

--- src/commands/owner/colormessage.ts
+++ src/commands/owner/colormessage.ts
@@ -1,6 +1,8 @@
 import { BotCommand } from '../../lib/extensions/BotCommand';
 import { MessageEmbed, MessageActionRow, MessageButton } from 'discord.js';
 
+
+const colorRole = ['Maya', 'Mikado', 'Rose', 'Lavender', 'Coral', 'Cantaloupe', 'Mint'];
 export default class color_message_command extends BotCommand {
 	constructor() {
 		super('color_message_setup', {
@@ -11,27 +13,68 @@ export default class color_message_command extends BotCommand {
 	}
 
 	async exec(message, args) {
+		// eslint-disable-next-line prefer-const
+		let roleArray: string[] = []
+		colorRole.forEach(rolename => {
+			const role = message.guild.roles.cache.find(
+				(role) => role.name === rolename
+			);
+			const rolePingString = `<@&${role.id}>\n`;
+			roleArray.push(rolePingString);
+		});
+		const displayRoleString = roleArray.join(' ');
 		const embed = new MessageEmbed()
 			.setColor('#36393f')
 			.setTitle('Choix de couleur.')
 			.setDescription(
-				'Clique sur un bouton pour avoir\n la couleur de ton choix.'
+				`Clique sur un bouton pour avoir\n la couleur de ton choix.\n${displayRoleString}`
 			);
 
-		const row = new MessageActionRow()
+		const row1 = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
-					.setCustomID('primary')
-					.setLabel('primary')
+					.setCustomID('maya')
+					.setLabel('Maya')
 					.setStyle('PRIMARY')
 			)
 			.addComponents(
 				new MessageButton()
-					.setCustomID('second')
-					.setLabel('second')
+					.setCustomID('mikado')
+					.setLabel('Mikado')
+					.setStyle('PRIMARY')
+			)
+			.addComponents(
+				new MessageButton()
+					.setCustomID('rose')
+					.setLabel('Rose')
+					.setStyle('PRIMARY')
+			)
+			.addComponents(
+				new MessageButton()
+					.setCustomID('lavender')
+					.setLabel('Lavender')
+					.setStyle('PRIMARY')
+		)
+		const row2 = new MessageActionRow()			
+			.addComponents(
+				new MessageButton()
+					.setCustomID('coral')
+					.setLabel('Coral')
+					.setStyle('PRIMARY')
+			)
+			.addComponents(
+				new MessageButton()
+					.setCustomID('cantaloupe')
+					.setLabel('Cantaloupe')
+					.setStyle('PRIMARY')
+		  )
+		  .addComponents(
+				new MessageButton()
+					.setCustomID('mint')
+					.setLabel('Mint')
 					.setStyle('PRIMARY')
 			);
-
-		return message.channel.send({ embeds: [embed], components: [row] });
+			
+		return message.channel.send({ embeds: [embed], components: [row1, row2] });
 	}
 }
