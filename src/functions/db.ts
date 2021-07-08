a8d134ce5dab804219169fc378fab5de823abf72
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-08 11:51:28 -0400

    ooops

--- src/functions/db.ts
+++ src/functions/db.ts
@@ -1,4 +1,5 @@
 import monk from 'monk';
+import * as config from '../config/options';
 const db = monk(config.mongodb);
 export const activity = db.get('activity');
 activity.createIndex({ id: 1 }, { unique: true });
