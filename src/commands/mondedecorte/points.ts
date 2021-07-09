191022328313029d3ece2aa35dfdae49634da812
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-09 14:59:45 -0400

    Args + embed for points command

--- src/commands/mondedecorte/points.ts
+++ src/commands/mondedecorte/points.ts
@@ -1,34 +1,55 @@
 import { BotCommand } from '../../lib/extensions/BotCommand';
 import { activity } from '../../functions/db';
 import { checkActifRole } from '../../functions/actifrole';
+import { MessageEmbed } from 'discord.js';
 
 export default class pointsCommand extends BotCommand {
 	constructor() {
 		super('points', {
 			aliases: ['points', 'mypoints'],
-			channel: 'guild'
+			channel: 'guild',
+			args: [
+				{
+					id: 'member',
+					type: 'member',
+					default: (message) => message.member
+				}
+			]
 		});
 	}
 
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	async exec(message) {
+	async exec(message, args) {
 		if (message.guild.id != '324284116021542922') return;
-		const userInDB = await activity.findOne({ id: message.author.id });
+		const userInDB = await activity.findOne({ id: args.member.id });
+
 		if (userInDB == null) {
-			message.channel.send({
-				content:
-					"Tu n'a pas de points. \nCommence pas envoyer des message pour en avoir."
-			});
+			const embed = new MessageEmbed()
+				.setColor('#36393f')
+				.setAuthor(message.author.tag, message.author.avatarURL())
+				.setDescription(
+					`<@${args.member.id}> n'a pas de points. \nCommence pas envoyer des message pour en avoir.`
+				)
+				.setTimestamp();
+			message.reply({ embeds: [embed] });
 		} else {
 			if (userInDB.points == 1) {
-				message.channel.send({ content: `Tu a ${userInDB.points} point.` });
+				const embed = new MessageEmbed()
+					.setColor('#36393f')
+					.setAuthor(message.author.tag, message.author.avatarURL())
+					.setDescription(`<@${args.member.id}> a ${userInDB.points} point.`)
+					.setTimestamp();
+				message.reply({ embeds: [embed] });
 			} else {
-				message.channel.send({ content: `Tu a ${userInDB.points} points.` });
+				const embed = new MessageEmbed()
+					.setColor('#36393f')
+					.setAuthor(message.author.tag, message.author.avatarURL())
+					.setDescription(`<@${args.member.id}> a ${userInDB.points} points.`)
+					.setTimestamp();
+				message.reply({ embeds: [embed] });
 			}
 
 			checkActifRole(message.member, message.guild, userInDB.points);
 		}
-
-		
 	}
 }
