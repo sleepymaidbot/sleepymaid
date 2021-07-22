837edd59cc182994f6a2049355c79afa2bc2bf42
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-22 17:28:43 -0400

    Create onCommand.ts

---
+++ src/listeners/onCommand.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Listener } from "discord-akairo";
+import { Message } from "discord.js";
+
+export default class OnCommand extends Listener {
+    constructor() {
+        super("onCommand", {
+            event: "commandStarted",
+            emitter: "commandHandler"
+        });
+    }
+
+    exec(message: Message) {
+        return console.log(`${message.guild.name} > ${message.author.tag} > ${message.content}`);
+    }
+}
