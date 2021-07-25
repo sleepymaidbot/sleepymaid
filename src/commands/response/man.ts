ff2d775cc4f28fd1748d269d68c8fd7510542a5a
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-25 17:23:51 -0400

    remove slash

--- src/commands/response/man.ts
+++ src/commands/response/man.ts
@@ -1,33 +1,19 @@
 import { Message } from 'discord.js';
 import { BotCommand } from '../../lib/extensions/BotCommand';
-//import { CommandInteraction } from 'discord.js';
 
 export default class manCommand extends BotCommand {
 	constructor() {
 		super('man', {
 			aliases: ['man'],
-			category: 'response',
-			description: {
-				content: 'man',
-				usage: 'man',
-				examples: ['man']
-			}
+			category: 'response'
 		});
 	}
 
 	public async exec(message: Message): Promise<void> {
-		message.channel.send({
+		await message.channel.send({
 			files: [
 				'https://cdn.discordapp.com/attachments/436249478521946191/853683744568115220/20210425_110059.png'
 			]
 		});
 	}
-
-	/*public async execSlash(message: CommandInteraction): Promise<void> {
-		message.reply({
-			files: [
-				'https://cdn.discordapp.com/attachments/436249478521946191/853683744568115220/20210425_110059.png'
-			]
-		})
-	}*/
 }
