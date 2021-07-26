1a739cfa6853738da7044aff70b5fd78774a4bde
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-26 14:18:32 -0400

    rewrite hourpoints task

--- src/tasks/hourpoints.ts
+++ src/tasks/hourpoints.ts
@@ -1,23 +1,33 @@
 import { Task } from 'discord-akairo'
+import { TextChannel } from 'discord.js'
 import { userActivityModel } from '../functions/db'
 
 export default class pointsRemoveTask extends Task {
 	constructor() {
 		super('removePoints', {
-			delay: 1800000,
-			runOnStart: false
+			delay: 18000,
+			runOnStart: true
 		})
 	}
 
 	async exec() {
-		const guild = this.client.guilds.cache.get('324284116021542922')
+		
 
-		guild.members.cache.forEach(async (member) => {
-			const userInDB = await userActivityModel.findOne({ id: member.id })
-			if (userInDB != null && userInDB.points >= 1) {
-				userInDB.points = userInDB.points - 1
-				await userInDB.save()
-			}
+		await userActivityModel.find({}).then(async (docs) => {
+			docs.forEach(async (user) => {
+				const userInDB = await userActivityModel.findOne({ id: user.id })
+				if (userInDB != null && userInDB.points >= 1) {
+					userInDB.points = userInDB.points - 1
+					await userInDB.save()
+				}
+			})
+			const logChannel = this.client.channels.cache.get(
+				'863117686334554142'
+			) as TextChannel
+			const guild = this.client.guilds.cache.get('324284116021542922')
+			await logChannel.send(
+				`${guild.members.cache.size} members have been removed 1 activity points.`
+			)
 		})
 	}
 }
