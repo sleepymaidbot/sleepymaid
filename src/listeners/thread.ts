725a5c76b7798fff4164eb9bb8c1942f17b07b22
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-27 22:21:22 -0400

    format

--- src/listeners/thread.ts
+++ src/listeners/thread.ts
@@ -1,15 +1,15 @@
-import { ThreadChannel } from "discord.js";
-import { BotListener } from "../lib/extensions/BotListener";
+import { ThreadChannel } from 'discord.js'
+import { BotListener } from '../lib/extensions/BotListener'
 
 export default class autoJoinThread extends BotListener {
-    constructor() {
-        super('autoJoinThread', {
-            emitter: 'client',
+	constructor() {
+		super('autoJoinThread', {
+			emitter: 'client',
 			event: 'threadCreate'
-        })
-    }
+		})
+	}
 
-    async exec(thread: ThreadChannel) {
-        thread.join()
-    }
+	async exec(thread: ThreadChannel) {
+		thread.join()
+	}
 }
