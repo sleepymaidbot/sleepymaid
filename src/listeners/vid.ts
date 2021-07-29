690f6990d4d67374c601ea5d5f250f85473dce79
ecorte <antoine.turgeon2005@hotmail.com>
2021-07-29 14:43:22 -0400

    Enable vid.ts

--- src/listeners/vid.ts
+++ src/listeners/vid.ts
@@ -5,8 +5,6 @@ import fs from 'fs'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const execFile = util.promisify(require('child_process').execFile)
 
-const enabled = false
-
 const sites = [
 	'tiktok.com',
 	'https://redd.it',
@@ -28,7 +26,6 @@ export default class videoDL extends Listener {
 
 	async exec(message: Message) {
 		if (message.author.bot) return
-		if (!enabled) return
 
 		const argv = message.content.split(' ')
 
