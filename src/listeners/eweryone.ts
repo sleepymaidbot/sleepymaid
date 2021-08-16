f86a2ef4130d0efe6e9b9afe689e6bfebb205f6a
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-15 21:35:54 -0400

    Update eweryone.ts

--- src/listeners/eweryone.ts
+++ src/listeners/eweryone.ts
@@ -12,14 +12,17 @@ export default class eweryone extends Listener {
 
     async exec(message: Message) {
         if (message.content.match(/(@everyone)/)) {
+            if (message.member.roles.cache.has('876636031884087406')) return
             const role = message.guild.roles.cache.find(role => role.id === '876636031884087406')
             message.member.roles.add(role);
         } else if (message.content.match(/(remove h role)/)) {
-            message.channel.send({
-                content: `<@${message.author.id}>, okay, ive wemoved h role fwom u but pwease be mowre caweful next time (≧◡≦)`
-            })
-            const role = message.guild.roles.cache.find(role => role.id === '876636031884087406')
-            message.member.roles.remove(role);
+            if (message.member.roles.cache.has('876636031884087406')) {
+                message.channel.send({
+                    content: `<@${message.author.id}>, okay, ive wemoved h role fwom u but pwease be mowre caweful next time (≧◡≦)`
+                })
+                const role = message.guild.roles.cache.find(role => role.id === '876636031884087406')
+                message.member.roles.remove(role);
+            }
         }
     }
 }
