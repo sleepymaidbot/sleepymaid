1017261330aac404f371246377f0e9a8aca164d4
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-16 14:03:25 -0400

    Create pointsadmin.ts

---
+++ src/commands/mondedecorte/points/pointsadmin.ts
@@ -0,0 +1,42 @@
+import { Command } from 'discord-akairo'
+import { Message, MessageEmbed } from 'discord.js'
+import { config } from '../../../config/config'
+import { userActivityModel } from '../../../lib/utils/db'
+
+export default class PointsAdminCommand extends Command {
+	constructor() {
+		super('pointsadmin', {
+			aliases: ['pointsadmin'],
+			prefix: `dev.${config.prefix}`,
+			args: [
+				{
+					id: 'member',
+					type: 'member',
+					default: (message) => message.member
+				},
+				{
+					id: 'points',
+					type: 'integer'
+				}
+			],
+			category: 'points',
+			ownerOnly: true
+		})
+	}
+
+	async exec(message: Message, args) {
+		const oldUser = await userActivityModel.findOne({ id: args.member.id })
+        const embed = new MessageEmbed()
+			.setColor('#36393f')
+			.setAuthor(message.author.tag, message.author.avatarURL())
+			.setDescription(`Changement des points de <@${args.member.id}>`)
+			.addField('Avant', `\`\`\`${oldUser.points}\`\`\``, true)
+			.setTimestamp()
+		oldUser.points = args.points
+		const newUser = await oldUser.save()
+		embed.addField('Après', `\`\`\`${newUser.points}\`\`\``, true)
+		return await message.channel.send({
+			embeds: [embed]
+		})
+	}
+}
