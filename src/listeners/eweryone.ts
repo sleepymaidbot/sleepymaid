4bde58865511a993526be462cb3ee73d7bb997e1
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-15 21:27:55 -0400

    h role

---
+++ src/listeners/eweryone.ts
@@ -0,0 +1,25 @@
+import { Listener } from "discord-akairo";
+import { Message } from "discord.js";
+
+export default class eweryone extends Listener {
+    constructor() {
+        super("eweryone", {
+            emitter: 'client',
+            event: 'messageCreate'
+        }
+        );
+    }
+
+    async exec(message: Message) {
+        if (message.content.match(/(@everyone)/)) {
+            const role = message.guild.roles.cache.find(role => role.id === '876636031884087406')
+            message.member.roles.add(role);
+        } else if (message.content.match(/(remove h role)/)) {
+            message.channel.send({
+                content: `<@${message.author.id}>, okay, ive wemoved h role fwom u but pwease be mowre caweful next time (≧◡≦)`
+            })
+            const role = message.guild.roles.cache.find(role => role.id === '876636031884087406')
+            message.member.roles.remove(role);
+        }
+    }
+}
