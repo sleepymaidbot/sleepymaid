df4b72ded53b45b642063b488b9015b211661809
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-18 19:54:22 -0400

    Update db.ts

--- src/lib/utils/db.ts
+++ src/lib/utils/db.ts
@@ -10,43 +10,6 @@ export async function startDB(): Promise<void> {
 	})
 }
 
-// userActivity
-
-interface userActivity {
-	id: Snowflake
-	points: number
-}
-
-const userActivitySchema = new Schema<userActivity>(
-	{
-		id: { type: String, required: true, index: true },
-		points: { type: Number, required: true, default: 0 }
-	},
-	{ collection: 'activity' }
-)
-
-export const userActivityModel = model<userActivity>(
-	'activity',
-	userActivitySchema
-)
-
-// Custom role
-
-interface customRole {
-	id: Snowflake
-	role: Snowflake
-}
-
-const customRoleSchema = new Schema<customRole>(
-	{
-		id: { type: String, required: true, index: true },
-		role: { type: String, required: true, index: true }
-	},
-	{ collection: 'customRole' }
-)
-
-export const customRoleModel = model<customRole>('customRole', customRoleSchema)
-
 // mondecorte
 
 interface mondecorte {
