c64f52ca7db143810c9b8de5dce624b8e0e020d0
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-20 14:00:53 -0400

    Update vid.ts

--- src/listeners/vid.ts
+++ src/listeners/vid.ts
@@ -39,7 +39,8 @@ export default class videoDL extends Listener {
 						// eslint-disable-next-line @typescript-eslint/no-unused-vars
 						async (error, stdout, stderr) => {
 							if (error) {
-								throw error
+								console.error(error)
+								return
 							}
 							fileName = stdout.trim()
 							await execFile(
