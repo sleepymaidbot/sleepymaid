d23445b81a6d10f5432dace6df2605172d57e936
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-20 17:45:20 -0400

    Create banner.ts

---
+++ src/tasks/banner.ts
@@ -0,0 +1,30 @@
+import { Task } from 'discord-akairo'
+import { opendir } from 'fs/promises'
+
+export default class banner extends Task {
+	constructor() {
+		super('banner', {
+			delay: 3600000,
+			runOnStart: true
+		})
+	}
+
+	async exec() {
+		try {
+			const dir = await opendir('./banners')
+			const banners = []
+			for await (const dirent of dir) {
+				banners.push(dirent.name)
+			}
+
+			const banner = banners[Math.floor(Math.random() * banners.length)]
+
+			const guild = this.client.guilds.cache.get('324284116021542922')
+			guild
+				.setBanner(`./banners/${banner}`, `Changed banner to ${banner}`)
+				.catch(console.error)
+		} catch (err) {
+			console.error(err)
+		}
+	}
+}
