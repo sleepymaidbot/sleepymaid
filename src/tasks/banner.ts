434720eea8b1f588c0dfeb7c1bb46a3c865c6083
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-20 17:48:55 -0400

    Update banner.ts

--- src/tasks/banner.ts
+++ src/tasks/banner.ts
@@ -5,23 +5,22 @@ export default class banner extends Task {
 	constructor() {
 		super('banner', {
 			delay: 3600000,
-			runOnStart: true
+			runOnStart: false
 		})
 	}
 
 	async exec() {
 		try {
 			const dir = await opendir('./banners')
-			const banners = []
-			for await (const dirent of dir) {
-				banners.push(dirent.name)
-			}
+            const banners = []
+			for await (const dirent of dir) { 
+				if (dirent.name.endsWith('.png')) banners.push(dirent.name)
+            }
 
-			const banner = banners[Math.floor(Math.random() * banners.length)]
+			const banner = banners[Math.floor(Math.random()*banners.length)];
 
-			const guild = this.client.guilds.cache.get('324284116021542922')
-			guild
-				.setBanner(`./banners/${banner}`, `Changed banner to ${banner}`)
+            const guild = this.client.guilds.cache.get('324284116021542922')
+			guild.setBanner(`./banners/${banner}`, `Changed banner to ${banner}`)
 				.catch(console.error)
 		} catch (err) {
 			console.error(err)
