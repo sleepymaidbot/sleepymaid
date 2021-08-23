52b2c2ccfce8a8be11e6446a95b0be865a6e7bf4
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-23 17:28:50 -0400

    Urban command

---
+++ src/commands/fun/urban.ts
@@ -0,0 +1,23 @@
+import { Command } from 'discord-akairo'
+import axios from 'axios'
+
+export default class UrbanCommand extends Command {
+    constructor() {
+        super('urban', {
+            aliases: ['urban'],
+            cooldown: 20,
+            args: [
+                {
+                    id: 'search',
+                    type: 'string',
+                    match: 'content',
+                }
+            ]
+        })
+    }
+
+    async exec(message, args) {
+        const request = await axios.get(`https://api.urbandictionary.com/v0/define?term=${args.search}`)
+        await message.reply(`📚 Definitions for **${args.search}**\`\`\`fix\n${request.data.list[0].definition}\`\`\``)
+    }
+}
