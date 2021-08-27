bbf666e28cf1321c5b73100bd88ce261a2af54e2
ecorte <antoine.turgeon2005@hotmail.com>
2021-08-27 18:34:40 -0400

    lint

--- src/listeners/ryan.ts
+++ src/listeners/ryan.ts
@@ -1,24 +1,25 @@
-import { Listener } from 'discord-akairo';
-import { Message } from 'discord.js';
+import { Listener } from 'discord-akairo'
+import { Message } from 'discord.js'
 
 export default class RyanListener extends Listener {
-    public constructor() {
-        super('ryan', {
-            emitter: 'client',
-            event: 'messageCreate'
-        });
-    }
+	public constructor() {
+		super('ryan', {
+			emitter: 'client',
+			event: 'messageCreate'
+		})
+	}
 
-    public exec(message: Message): void {
-        if (message.content === '.' && message.author.id === '564122371385196546') {
-            const role = message.guild.roles.cache.find(r => r.id === '862462288345694210');
+	public exec(message: Message): void {
+		if (message.content === '.' && message.author.id === '564122371385196546') {
+			const role = message.guild.roles.cache.find(
+				(r) => r.id === '862462288345694210'
+			)
 
-            message.member.roles.add(role);
+			message.member.roles.add(role)
 
+			message.author.send('yo parle a ecorte sinon tu pu')
 
-            message.author.send('yo parle a ecorte sinon tu pu');
-
-            message.delete();
-        }
-    }
+			message.delete()
+		}
+	}
 }
