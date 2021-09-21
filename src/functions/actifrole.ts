05380f340daa284177a85eee8a1a60aa37e60170
ecorte <antoine.turgeon2005@hotmail.com>
2021-09-21 18:43:38 -0400

    FUCK

--- src/functions/actifrole.ts
+++ src/functions/actifrole.ts
@@ -12,7 +12,7 @@ export async function checkActifRole(
 	const userRole = member.roles.cache.map((role) => role.name)
 
 	if (member.id === guild.ownerId) return
-	if (member.bot) return
+	if (member.user.bot) return
 
 	if (points >= 100) {
 		if (!userRole.includes('Actif')) {
