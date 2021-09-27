cd2253a094a1c1153f8c22749e794391d8805c99
ecorte <antoine.turgeon2005@hotmail.com>
2021-09-27 17:37:06 -0400

    Update bannervote.ts

--- src/listeners/bannervote.ts
+++ src/listeners/bannervote.ts
@@ -5,6 +5,8 @@ module.exports = {
 		if (message.channel.id === '878370280941183016') {
 			message.react('✅')
 			message.react('❌')
+		} else if (message.channel.id === '892162546277187654') {
+			message.react('🔼')
 		}
 	}
 }
