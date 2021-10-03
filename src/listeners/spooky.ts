66e0831071f40b23370b5a86cb71348713951681
ecorte <antoine.turgeon2005@hotmail.com>
2021-10-03 16:52:07 -0400

    Update spooky.ts

--- src/listeners/spooky.ts
+++ src/listeners/spooky.ts
@@ -3,14 +3,15 @@ module.exports = {
 	once: false,
 	async execute(interaction) {
 		if (interaction.customId == 'spooky') {
-			if (!interaction.member.roles.cache.has('894308407069274163')) {
+            const role = await interaction.guild.roles.cache.find(
+                (r) => r.id === '894308407069274163'
+            )
+			if (interaction.member.roles.cache.has('894308407069274163')) {
 				await interaction.deferUpdate()
-				const role = interaction.guild.roles.cache.find(
-					(r) => r.id === '894308407069274163'
-				)
-				await interaction.member.roles.add(role)
+				await interaction.member.roles.remove(role)
 			} else {
 				await interaction.deferUpdate()
+                await interaction.member.roles.add(role)
 			}
 		}
 	}
