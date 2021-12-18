4f5ec9c54417dcf94d4ff33fb579d496b018ef15
ecorte <antoine.turgeon2005@hotmail.com>
2021-12-18 01:23:04 -0500

    Add wordFilter

---
+++ src/listeners/autoMod/wordFilter.ts
@@ -0,0 +1,21 @@
+import { Message } from 'discord.js'
+
+module.exports = {
+	name: 'messageCreate',
+	once: false,
+
+	async execute(message: Message) {
+		if (message.author.id === '324281236728053760') return
+        if (message.guild.id !== '324284116021542922') return
+		const str = message.content.replace(/ /g, '')
+		str.replace(/‎ /g, '')
+		if (message.author.bot) return
+		if (
+			str.match(
+				/nig|neg|nag|nég|nezg|nlg|igg|n1g|ggr0|ni2|n!g|n?g|n*g|ngg|ng|n¡g|niq/gim
+			)
+		) {
+			await message.delete()
+		}
+	}
+}
