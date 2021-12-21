56f7a197e0a847ce17e0e80bfb3037318bbe6209
ecorte <antoine.turgeon2005@hotmail.com>
2021-12-21 15:25:14 -0500

    Update bannervote.ts

--- src/listeners/lmeInteractions/bannervote.ts
+++ src/listeners/lmeInteractions/bannervote.ts
@@ -1,7 +1,10 @@
+const userBl = ['324281236728053760', '613040835684073506', '599315471640297502']
+
 module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(message) {
+		if (userBl.includes(message.author.id)) return
 		if (message.channel.id === '878370280941183016') {
 			message.react('✅')
 			message.react('❌')
