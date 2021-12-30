1b4e1d89ab367021eba30165501da4ad74eadb07
Antoine <antoine.turgeon2005@hotmail.com>
2021-12-30 15:39:40 -0500

    Update wordFilter.ts

--- src/listeners/autoMod/wordFilter.ts
+++ src/listeners/autoMod/wordFilter.ts
@@ -1,5 +1,8 @@
 import { Message } from 'discord.js'
 
+const blacklistRegex = /nig|neg|nag|nég|nezg|nlg|n1g|ggr0|ni2|n!g|ngg|n¡g|niq/gim
+const whitelistRegex = /negatif|négatif/gim
+
 module.exports = {
 	name: 'messageCreate',
 	once: false,
@@ -7,10 +10,19 @@ module.exports = {
 	async execute(message: Message) {
 		if (message.author.id === '324281236728053760') return
 		if (message.guild.id !== '324284116021542922') return
+		if (message.author.bot) return
 		const str = message.content.replace(/ /g, '')
 		str.replace(/‎ /g, '')
-		if (message.author.bot) return
-		if (str.match(/nig|neg|nag|nég|nezg|nlg|n1g|ggr0|ni2|n!g|ngg|n¡g|niq/gim)) {
+		const wordArray = message.content.split(' ')
+		let infractions = 0
+		wordArray.forEach((word) => {
+			if (word.match(blacklistRegex)) {
+				if (word.match(whitelistRegex)) return
+				infractions = infractions + 1
+			}
+		})
+		if (str.match(blacklistRegex)) infractions = infractions + 1
+		if (infractions >= 1) {
 			await message.delete()
 			await message.member.timeout(60 * 1000, 'Triggered word filter')
 		}
