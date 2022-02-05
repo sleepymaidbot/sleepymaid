489e9a759f99d976fe1f28629b3cc7b1976280be
ecorte <antoine.turgeon2005@hotmail.com>
2022-02-05 10:32:33 -0500

    feat(read/BotClient): cache all member at start

--- src/listeners/Misc/ready.ts
+++ src/listeners/Misc/ready.ts
@@ -6,5 +6,15 @@ module.exports = {
 		await client.logger.info(
 			`Logged in as ${client.user.tag} | ${client.guilds.cache.size} servers`
 		)
+
+		await client.guilds
+			.fetch()
+			.then(async (guilds) => {
+				for (const guild of guilds.values()) {
+					const g = await client.guilds.fetch(guild.id)
+					await g.members.fetch().catch((e) => client.logger.error(e))
+				}
+			})
+			.catch((e) => client.logger.error(e))
 	}
 }
