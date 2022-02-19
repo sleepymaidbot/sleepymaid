5962763116c64ae41ef3e2c857a5f6e700a4a37c
ecorte <antoine.turgeon2005@hotmail.com>
2022-02-18 20:21:34 -0500

    feat: readd drrazzz welcome

---
+++ services/bot/src/listeners/drrazzz/welcome.ts
@@ -0,0 +1,37 @@
+import { Embed } from '@discordjs/builders'
+
+module.exports = {
+	name: 'guildMemberAdd',
+	once: false,
+
+	async execute(member) {
+		if (member.guild.id !== '818313526720462868') return
+		const role = await member.guild.roles.cache.get('818475324631023656')
+		await member.roles.add(role)
+
+		const ruleChannel = await member.guild.channels.cache.get(
+			'818314179508568126'
+		)
+		const generalChannel = await member.guild.channels.cache.get(
+			'818313526720462870'
+		)
+
+		const embed = new Embed()
+			.setAuthor({
+				name: 'Bienvenue!',
+				iconURL: 'https://cdn.discordapp.com/emojis/612355003151286278.gif'
+			})
+			.setDescription(
+				`Bienvenue ${member} sur le serveur de DrraZz_.\nJe te conseil d'aller lire les règle du serveur dans <#818314179508568126>.\nSi tu veux avoir des notification quand <@377944202710876161> va en live sur twitch ou sort une nouvelle vidéo sur youtube va dans <#818474275362963486>.`
+			)
+			.setColor(0x36393f)
+
+		await generalChannel.send({ embeds: [embed] })
+		const ruleMessage = await ruleChannel.send({
+			content: `${member} Merci de lire les règlements!`
+		})
+		setTimeout(async () => {
+			await ruleMessage.delete()
+		}, 10000)
+	}
+}
