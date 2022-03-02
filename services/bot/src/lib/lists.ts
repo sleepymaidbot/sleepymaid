510ee6da167dc8cec012ca99ba2656589965e9d5
ecorte <antoine.turgeon2005@hotmail.com>
2022-03-02 12:07:54 -0500

    feat(list): add self role ids

--- services/bot/src/lib/lists.ts
+++ services/bot/src/lib/lists.ts
@@ -10,6 +10,24 @@ export const colorRole = [
 	'Weed'
 ]
 
+export const colorRoleIds = [
+	'857372101748719656', // Maya
+	'857372291855679538', // Mikado
+	'857372400440967198', // Rose
+	'857372585552773120', // Lavender
+	'857372666141736981', // Coral
+	'857372789139963925', // Cantaloupe
+	'857372929598947368', // Mint
+	'857431586202189835', // Smoked
+	'857432207534981151' // Weed
+]
+
+export const pingRoleIds = [
+	'879465272669528098', // Annonce
+	'879465303795466240', // Free Stuff
+	'879465436922642462' // Giveaway
+]
+
 export const ColorfulNeedRole = [
 	'398478742709600256', // staff
 	'719221506047213638', // nitro booster
