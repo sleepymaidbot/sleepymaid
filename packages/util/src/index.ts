c3c4498b09758aa23309f1716f9cdf4deb4620ae
ecorte <antoine.turgeon2005@hotmail.com>
2022-03-12 12:38:08 -0500

    feat(utils): remove deepEquals

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,60 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { readdirSync } from 'fs'
 
 export default class Util {
-	public static deepEquals<T>(
-		a: unknown,
-		b: T,
-		options?: DeepEqualsOptions
-	): a is T
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public static deepEquals(
-		a: any,
-		b: any,
-		options?: DeepEqualsOptions
-	): boolean {
-		const { ignoreUndefined = true, ignoreArrayOrder = true } = options ?? {}
-
-		if (a === b) return true
-		if (typeof a !== 'object' || typeof b !== 'object')
-			throw new TypeError('Not objects')
-		if (
-			(Array.isArray(a) && !Array.isArray(b)) ||
-			(!Array.isArray(a) && Array.isArray(b))
-		)
-			return false
-		const newA =
-			ignoreArrayOrder &&
-			Array.isArray(a) &&
-			a.length &&
-			typeof a[0] !== 'object'
-				? [...a].sort()
-				: a
-		const newB =
-			ignoreArrayOrder &&
-			Array.isArray(b) &&
-			b.length &&
-			typeof b[0] !== 'object'
-				? [...b].sort()
-				: b
-		for (const key in newA) {
-			if (ignoreUndefined && newA[key] === undefined && newB[key] === undefined)
-				continue
-			if (!(key in newB)) return false
-			if (typeof newA[key] === 'object' && typeof newB[key] === 'object') {
-				if (
-					!this.deepEquals(newA[key], newB[key], {
-						ignoreUndefined,
-						ignoreArrayOrder
-					})
-				)
-					return false
-			} else if (newA[key] !== newB[key]) return false
-		}
-		return true
-	}
-
-	public static async loadFolder(folder: string) {
+	public static async loadFolder(folder: string): Promise<string[]> {
 		const fsfolder = readdirSync(folder)
 		const files = []
 		for (const file of fsfolder) {
@@ -67,9 +14,4 @@ export default class Util {
 		}
 		return files
 	}
-}
-
-export interface DeepEqualsOptions {
-	ignoreUndefined?: boolean
-	ignoreArrayOrder?: boolean
 }
