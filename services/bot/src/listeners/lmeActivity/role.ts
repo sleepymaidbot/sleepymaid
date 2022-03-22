96045a8e0baf47915005bccf0cc9854f254773fd
ecorte <antoine.turgeon2005@hotmail.com>
2022-03-22 18:29:10 -0400

    feat(role): ignore bot and move folder

--- services/bot/src/listeners/lmeActivity/role.ts
+++ services/bot/src/listeners/lmeActivity/role.ts
@@ -2,12 +2,18 @@ import { roleSyncer } from '../../lib/rolesyncer'
 import 'reflect-metadata'
 import { container } from 'tsyringe'
 import { BotClient } from '../../lib/BotClient'
+import { GuildMember } from 'discord.js'
 
 module.exports = {
 	name: 'guildMemberUpdate',
 	once: false,
 
-	async execute(oldMember, newMember, client: BotClient) {
+	async execute(
+		oldMember: GuildMember,
+		newMember: GuildMember,
+		client: BotClient
+	) {
+		if (newMember.user.bot) return
 		container.register(BotClient, { useValue: client })
 		await container.resolve(roleSyncer).checkUserRole(newMember)
 	}
