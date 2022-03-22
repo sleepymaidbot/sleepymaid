14d908aa3d6299e48977f60d46ff1a156efe59cd
ecorte <antoine.turgeon2005@hotmail.com>
2022-03-22 18:48:53 -0400

    feat(prisma): wrong names

---
+++ prisma/migrations/20220322224541_wrong_names/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "leveling_track" ALTER COLUMN "globalMultiplier" SET DEFAULT 1;
+
+-- AlterTable
+ALTER TABLE "track_channel_multiplier" ALTER COLUMN "multiplier" SET DEFAULT 1;
+
+-- AlterTable
+ALTER TABLE "track_role_multiplier" ALTER COLUMN "multiplier" SET DEFAULT 1;
