fffb73daa98110b3f35058f6efecb835e598f176
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-07 20:40:48 -0400

    test(Handler): add message command test

---
+++ services/testbot/src/commands/message/content.ts
@@ -0,0 +1,25 @@
+import { ContextMenuCommandBuilder } from '@discordjs/builders'
+import { MessageCommand } from '@sleepymaid-ts/handler'
+import { ApplicationCommandType } from 'discord-api-types/v10'
+import { MessageApplicationCommandData } from 'discord.js'
+
+export default new MessageCommand(
+	{
+		guildIds: ['821717486217986098'],
+		data: new ContextMenuCommandBuilder()
+			.setName('Get Message Content')
+			.setType(ApplicationCommandType.Message)
+			.toJSON() as MessageApplicationCommandData
+	},
+	{
+		run: async (interaction, client) => {
+			try {
+				interaction.reply(
+					`The content of the targeted message is ${interaction.targetMessage.content}`
+				)
+			} catch (error) {
+				client.logger.error(error)
+			}
+		}
+	}
+)
