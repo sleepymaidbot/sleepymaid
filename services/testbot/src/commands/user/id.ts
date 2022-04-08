ba1cb97d9750b56279417469fa80cd5da2a347b1
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-07 20:40:26 -0400

    test(Handler): add user command test

---
+++ services/testbot/src/commands/user/id.ts
@@ -0,0 +1,33 @@
+import { HandlerClient, UserCommand } from '@sleepymaid-ts/handler'
+import {
+	UserApplicationCommandData,
+	UserContextMenuCommandInteraction
+} from 'discord.js'
+import { ContextMenuCommandBuilder } from '@discordjs/builders'
+import { ApplicationCommandType } from 'discord-api-types/v10'
+
+export default new UserCommand(
+	{
+		guildIds: ['821717486217986098'],
+		data: new ContextMenuCommandBuilder()
+			.setName('Get User Id')
+			.setType(ApplicationCommandType.User)
+			.toJSON() as UserApplicationCommandData
+	},
+	{
+		run: async (
+			interaction: UserContextMenuCommandInteraction,
+			client: HandlerClient
+		) => {
+			try {
+				interaction.reply({
+					content: `The user ID is ${interaction.targetUser.id}`,
+					ephemeral: true
+				})
+				client.logger.info(`Pong!`)
+			} catch (error) {
+				client.logger.error(error)
+			}
+		}
+	}
+)
