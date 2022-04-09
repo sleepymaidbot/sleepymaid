4b405c11ad276eef9c5881173b0c146a89e37c8d
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-09 16:56:10 -0400

    feat(Tasks): migrate tasks to handler

--- services/bot/src/tasks/activity/hourpoints.ts
+++ services/bot/src/tasks/activity/hourpoints.ts
@@ -4,52 +4,57 @@ import { pointToRemoveForPoints } from '../../lib/lists'
 import { ActivityRewardManager } from '../../lib/activityRewardManager'
 import { container } from 'tsyringe'
 import { BotClient } from '../../lib/BotClient'
+import { Task } from '@sleepymaid/handler'
 
-module.exports = {
-	interval: 3600000,
-
-	async execute(client: BotClient) {
-		return
-		client.logger.debug('Hourpoints task started')
-		const usersArray = []
-		const docs = await client.prisma.mondecorte.findMany()
-		const guild = await client.guilds.cache.get('324284116021542922')
-		for (const user of docs) {
-			const userInDb = await client.prisma.mondecorte.findUnique({
-				where: { user_id: user.user_id }
-			})
-			if (userInDb != null && userInDb.points >= 1) {
-				let pointsToLoose = 1
-				pointToRemoveForPoints.forEach((e) => {
-					if (e.need <= userInDb.points) pointsToLoose = e.remove
+export default new Task(
+	{
+		interval: 3600000
+	},
+	{
+		async run(client: BotClient) {
+			client.logger.debug('Hourpoints task started')
+			const usersArray = []
+			const docs = await client.prisma.mondecorte.findMany()
+			const guild = client.guilds.cache.get('324284116021542922')
+			for (const user of docs) {
+				const userInDb = await client.prisma.mondecorte.findUnique({
+					where: { user_id: user.user_id }
 				})
+				if (userInDb != null && userInDb.points >= 1) {
+					let pointsToLoose = 1
+					pointToRemoveForPoints.forEach((e) => {
+						if (e.need <= userInDb.points) pointsToLoose = e.remove
+					})
 
-				const newPoints = userInDb.points - pointsToLoose
-				await client.prisma.mondecorte.update({
-					where: { user_id: user.user_id },
-					data: { points: newPoints }
-				})
-				usersArray.push(user.user_id)
+					const newPoints = userInDb.points - pointsToLoose
+					await client.prisma.mondecorte.update({
+						where: { user_id: user.user_id },
+						data: { points: newPoints }
+					})
+					usersArray.push(user.user_id)
 
-				try {
-					const dUser = await guild.members.cache.get(user.user_id)
-					if (dUser) {
-						container.register(BotClient, { useValue: client })
-						container.resolve(ActivityRewardManager).checkActivityReward(dUser)
+					try {
+						const dUser = await guild.members.cache.get(user.user_id)
+						if (dUser) {
+							container.register(BotClient, { useValue: client })
+							container
+								.resolve(ActivityRewardManager)
+								.checkActivityReward(dUser)
+						}
+					} catch (e) {
+						client.logger.error(e)
 					}
-				} catch (e) {
-					client.logger.error(e)
 				}
+				const logChannel = client.channels.cache.get(
+					'863117686334554142'
+				) as TextChannel
+				await logChannel.send({
+					content: `**Hourly points**\n${usersArray.length} members have been removed activity points.`
+				})
+				client.logger.info(
+					`${usersArray.length} members have been removed activity points.`
+				)
 			}
 		}
-		const logChannel = (await client.channels.cache.get(
-			'863117686334554142'
-		)) as TextChannel
-		await logChannel.send({
-			content: `**Hourly points**\n${usersArray.length} members have been removed activity points.`
-		})
-		client.logger.info(
-			`${usersArray.length} members have been removed activity points.`
-		)
 	}
-}
+)
--- services/bot/src/tasks/lme/banner.ts
+++ services/bot/src/tasks/lme/banner.ts
@@ -1,32 +1,40 @@
 import { opendir } from 'fs/promises'
 import { config } from '@sleepymaid/config'
+import { Task } from '@sleepymaid/handler'
+import { BotClient } from '../../lib/BotClient'
+import { TextChannel } from 'discord.js'
 
-module.exports = {
-	interval: 3600000,
+export default new Task(
+	{
+		interval: 3600000
+	},
+	{
+		async run(client: BotClient) {
+			client.logger.debug('Banner task started')
+			if (config.isDevelopment) return
+			const guild = client.guilds.cache.get('324284116021542922')
+			if (guild.premiumSubscriptionCount < 7) return
+			try {
+				const dir = await opendir('./banners')
+				const banners = []
+				for await (const dirent of dir) {
+					if (dirent.name.endsWith('.png')) banners.push(dirent.name)
+				}
 
-	async execute(client) {
-		client.logger.debug('Banner task started')
-		if (config.isDevelopment) return
-		const guild = client.guilds.cache.get('324284116021542922')
-		if (guild.premiumSubscriptionCount < 7) return
-		try {
-			const dir = await opendir('./banners')
-			const banners = []
-			for await (const dirent of dir) {
-				if (dirent.name.endsWith('.png')) banners.push(dirent.name)
+				const banner = banners[Math.floor(Math.random() * banners.length)]
+
+				guild
+					.setBanner(`./banners/${banner}`, `Changed banner to ${banner}`)
+					.catch(console.error)
+
+				const channel = guild.channels.cache.get(
+					'863117686334554142'
+				) as TextChannel
+
+				channel.send(`**Banner Rotation**\nBanner is now \`\`${banner}\`\``)
+			} catch (err) {
+				console.error(err)
 			}
-
-			const banner = banners[Math.floor(Math.random() * banners.length)]
-
-			guild
-				.setBanner(`./banners/${banner}`, `Changed banner to ${banner}`)
-				.catch(console.error)
-
-			guild.channels.cache
-				.get('863117686334554142')
-				.send(`**Banner Rotation**\nBanner is now \`\`${banner}\`\``)
-		} catch (err) {
-			console.error(err)
 		}
 	}
-}
+)
