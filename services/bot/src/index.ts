256d2536210208d4c321a4bfd7c6a57496a65b77
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-09 17:21:26 -0400

    refactor(Bot): use top level async for start

--- services/bot/src/index.ts
+++ services/bot/src/index.ts
@@ -1,5 +1,23 @@
+import { config } from '@sleepymaid/config'
+import { resolve } from 'path'
 import { BotClient } from './lib/BotClient'
+;(async () => {
+	const client: BotClient = new BotClient()
 
-const client: BotClient = new BotClient()
+	client.on('ready', async () => {
+		await client.loadHandlers({
+			commands: {
+				folder: resolve(__dirname, './slashCommands')
+			},
+			listeners: {
+				folder: resolve(__dirname, './listeners')
+			},
+			tasks: {
+				folder: resolve(__dirname, './tasks')
+			}
+		})
+		await client.registerApplicationCommandsPermissions()
+	})
 
-client.startAll()
+	await client.login(config.token)
+})()
