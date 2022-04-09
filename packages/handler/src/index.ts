da4e715b272cff58d767adf6f6bab05bfe2e47b8
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-09 12:21:26 -0400

    refactor(Handler): export with *

--- packages/handler/src/index.ts
+++ packages/handler/src/index.ts
@@ -1,65 +1,25 @@
-export { HandlerClient } from './HandlerClient'
-export type {
-	env,
-	HandlerClientOptions,
-	loadHandlersOptions,
-	Logger,
-	LogFn
-} from './HandlerClient'
+export * from './HandlerClient'
 
-export { Logger as BaseLogger } from './Logger'
+export * from './BaseLogger'
 
 // ./commands
 
-export { SlashCommand } from './commands/SlashCommand'
-export type {
-	SlashCommandInfo,
-	SlashCommandData,
-	CommandRunFunction,
-	AutoCompleteFunction,
-	SlashCommandFunctionType
-} from './commands/SlashCommand'
+export * from './commands/SlashCommand'
 
-export { UserCommand } from './commands/UserCommand'
-export type {
-	UserCommandInfo,
-	UserCommandRunFunction,
-	UserCommandFunctionType
-} from './commands/UserCommand'
+export * from './commands/UserCommand'
 
-export { MessageCommand } from './commands/MessageCommand'
-export type {
-	MessageCommandInfo,
-	MessageCommandRunFunction,
-	MessageCommandFunctionType
-} from './commands/MessageCommand'
+export * from './commands/MessageCommand'
 
-export { CommandManager } from './commands/CommandManager'
-export type {
-	CommandManagerStartAllOptionsType,
-	GuildCommandsType
-} from './commands/CommandManager'
+export * from './commands/CommandManager'
 
 // ./listeners
 
-export { Listener } from './listeners/Listener'
-export type {
-	ListenerInfo,
-	FunctionType as ListenerFunctionType,
-	ListenerRunFunction
-} from './listeners/Listener'
+export * from './listeners/Listener'
 
-export { ListenerManager } from './listeners/ListenerManager'
-export type { ListenerManagerStartAllOptionsType } from './listeners/ListenerManager'
+export * from './listeners/ListenerManager'
 
 // ./tasks
 
-export { Task } from './tasks/Task'
-export type {
-	TaskInfo,
-	FunctionType as TaskFunctionType,
-	TaskRunFunction
-} from './tasks/Task'
+export * from './tasks/Task'
 
-export { TaskManager } from './tasks/TaskManager'
-export type { TaskManagerStartAllOptionsType } from './tasks/TaskManager'
+export * from './tasks/TaskManager'
