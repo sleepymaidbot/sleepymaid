35c114ef265c18f2f84a4a3a5f00e83271f57568
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-15 21:14:48 -0400

    fix(testbot): update to new handler version

--- services/testbot/src/index.ts
+++ services/testbot/src/index.ts
@@ -18,22 +18,23 @@ const client = new HandlerClient(
 	}
 )
 
+client.loadHandlers({
+	commands: {
+		folder: resolve(__dirname, './commands')
+	},
+	listeners: {
+		folder: resolve(__dirname, './listeners')
+	},
+	tasks: {
+		folder: resolve(__dirname, './tasks')
+	}
+})
+
 client.on('ready', async () => {
 	client.logger.info('Ready!')
 	/*await client.loadCommands(resolve(__dirname, './commands'))
 	await client.loadListeners(resolve(__dirname, './listeners'))
 	await client.loadTasks(resolve(__dirname, './tasks'))*/
-	await client.loadHandlers({
-		commands: {
-			folder: resolve(__dirname, './commands')
-		},
-		listeners: {
-			folder: resolve(__dirname, './listeners')
-		},
-		tasks: {
-			folder: resolve(__dirname, './tasks')
-		}
-	})
 })
 
 client.login(config.token)
--- services/testbot/src/listeners/ready.ts
+++ services/testbot/src/listeners/ready.ts
@@ -1,12 +1,20 @@
-import { Listener } from '@sleepymaid/handler'
+import { HandlerClient, Listener } from '@sleepymaid/handler'
+import { Guild } from 'discord.js'
 export default new Listener(
 	{
-		name: 'interactionCreate',
+		name: 'ready',
 		once: false
 	},
 	{
-		run: async function run(client, interaction) {
-			client.logger.info('Listener ran', interaction)
+		run: async function run(client: HandlerClient) {
+			client.logger.info('Listener ran')
+			const guilds = await client.guilds.fetch()
+			for (const guild of guilds.values()) {
+				const g: Guild = await client.guilds.fetch(guild.id)
+
+				await g.members.fetch().catch((e) => client.logger.error(e))
+			}
+			client.logger.info('in guilds', client.guilds.cache.size.toString())
 		}
 	}
 )
