17cc980291ffacbbd0ece07f387542876b5ef0d0
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-15 21:16:02 -0400

    refactor(ratio): use localizer

--- services/bot/src/slashCommands/chat/ratio.ts
+++ services/bot/src/slashCommands/chat/ratio.ts
@@ -1,13 +1,17 @@
 import { SlashCommand } from '@sleepymaid/handler'
 import {
 	ApplicationCommandOptionType,
-	ApplicationCommandType,
-	CommandInteraction
+	ApplicationCommandType
 } from 'discord.js'
+import { BotClient } from '../../lib/BotClient'
 
 export default new SlashCommand(
 	{
-		guildIds: ['324284116021542922', '860721584373497887'],
+		guildIds: [
+			'324284116021542922',
+			'860721584373497887',
+			'821717486217986098'
+		],
 		data: {
 			name: 'ratio',
 			description: 'Rationalise une personne',
@@ -23,10 +27,15 @@ export default new SlashCommand(
 		}
 	},
 	{
-		async run(interaction: CommandInteraction) {
+		async run(interaction, client: BotClient) {
 			const user = interaction.options.get('user')
-			interaction.reply(
-				`Ratio à <@${user.value}> par <@${interaction.member.user.id}>.`
+
+			await interaction.reply(
+				await client.localizer.get('ratio', {
+					lng: interaction.locale,
+					target: `<@${user.value}>`,
+					author: `<@${interaction.user.id}>`
+				})
 			)
 		}
 	}
