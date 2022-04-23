ee6fc3c5ccd52c38ae6daceeebae56a65a7e9d95
Antoine <antoine.turgeon2005@hotmail.com>
2022-04-23 15:04:37 -0400

    feat(testbot): use new config

--- services/testbot/src/index.ts
+++ services/testbot/src/index.ts
@@ -1,40 +1,42 @@
-import { config } from '@sleepymaid/config'
+import { ConfigManager } from '@sleepymaid/config'
 import { HandlerClient } from '@sleepymaid/handler'
 import { GatewayIntentBits } from 'discord.js'
 import { resolve } from 'path'
+;(async () => {
+	const client = new HandlerClient(
+		{
+			devServerId: '821717486217986098'
+		},
+		{
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMembers,
+				GatewayIntentBits.GuildBans,
+				GatewayIntentBits.GuildVoiceStates,
+				GatewayIntentBits.GuildMessages
+			]
+		}
+	)
 
-const client = new HandlerClient(
-	{
-		devServerId: '821717486217986098'
-	},
-	{
-		intents: [
-			GatewayIntentBits.Guilds,
-			GatewayIntentBits.GuildMembers,
-			GatewayIntentBits.GuildBans,
-			GatewayIntentBits.GuildVoiceStates,
-			GatewayIntentBits.GuildMessages
-		]
-	}
-)
+	client.loadHandlers({
+		commands: {
+			folder: resolve(__dirname, './commands')
+		},
+		listeners: {
+			folder: resolve(__dirname, './listeners')
+		},
+		tasks: {
+			folder: resolve(__dirname, './tasks')
+		}
+	})
 
-client.loadHandlers({
-	commands: {
-		folder: resolve(__dirname, './commands')
-	},
-	listeners: {
-		folder: resolve(__dirname, './listeners')
-	},
-	tasks: {
-		folder: resolve(__dirname, './tasks')
-	}
-})
+	client.on('ready', async () => {
+		client.logger.info('Ready!')
+	})
 
-client.on('ready', async () => {
-	client.logger.info('Ready!')
-	/*await client.loadCommands(resolve(__dirname, './commands'))
-	await client.loadListeners(resolve(__dirname, './listeners'))
-	await client.loadTasks(resolve(__dirname, './tasks'))*/
-})
+	const configManager = new ConfigManager()
 
-client.login(config.token)
+	const configs = await configManager.initConfig()
+
+	await client.login(configs['bot'].token)
+})().catch(console.error)
