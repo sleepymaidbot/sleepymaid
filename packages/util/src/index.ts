0340bee1a1b28d98b15201e21aad761bc9e68051
Antoine <antoine.turgeon2005@hotmail.com>
2022-05-20 13:56:20 -0400

    chore(Utils): add jsdoc

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -3,6 +3,11 @@ import { readdirSync } from 'fs'
 import { promisify } from 'util'
 
 export default class Util {
+	/**
+	 *
+	 * @param folder The folder to read
+	 * @returns The files in the folder and subfolders
+	 */
 	public static async loadFolder(folder: string): Promise<string[]> {
 		const fsfolder = readdirSync(folder)
 		const files = []
@@ -17,6 +22,11 @@ export default class Util {
 		return files
 	}
 
+	/**
+	 * Runs a shell command and gives the output
+	 * @param command The shell command to run
+	 * @returns The stdout and stderr of the shell command
+	 */
 	public async shell(command: string): Promise<{
 		stdout: string
 		stderr: string
