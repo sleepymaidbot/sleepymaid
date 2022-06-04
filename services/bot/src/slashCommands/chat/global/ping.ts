8af0d0511f3a8b5d0c7088cb66d4bd5f090137ba
Antoine <antoine.turgeon2005@hotmail.com>
2022-06-04 01:01:26 -0400

    refactor(ping): move folder

--- services/bot/src/slashCommands/chat/global/ping.ts
+++ services/bot/src/slashCommands/chat/global/ping.ts
@@ -5,7 +5,7 @@ import {
 	EmbedBuilder,
 	Message
 } from 'discord.js'
-import { BotClient } from '../../lib/extensions/BotClient'
+import { BotClient } from '../../../lib/extensions/BotClient'
 
 export default new SlashCommand(
 	{
