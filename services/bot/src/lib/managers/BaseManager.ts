d5d93034a8406edd273e59f9dff75ab1553deb24
Antoine <antoine.turgeon2005@hotmail.com>
2022-06-16 21:16:57 -0400

    Fix(BaseManager): make basemanager a singleton

--- services/bot/src/lib/managers/BaseManager.ts
+++ services/bot/src/lib/managers/BaseManager.ts
@@ -1,5 +1,7 @@
+import { singleton } from 'tsyringe'
 import { BotClient } from '../extensions/BotClient'
 
+@singleton()
 export class baseManager {
 	public declare client: BotClient
 	constructor(client: BotClient) {
