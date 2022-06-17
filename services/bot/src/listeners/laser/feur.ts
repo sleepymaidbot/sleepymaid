61a59eca57fe0a837b18bc00012d26ea3942b87e
Antoine <antoine.turgeon2005@hotmail.com>
2022-06-16 21:17:52 -0400

    refactor(LaserFeur): only run in prod

--- services/bot/src/listeners/laser/feur.ts
+++ services/bot/src/listeners/laser/feur.ts
@@ -1,5 +1,6 @@
 import { Listener } from '@sleepymaid/handler'
 import { Message } from 'discord.js'
+import { BotClient } from '../../lib/extensions/BotClient'
 
 const triggers = ['quoi', 'quoi?', 'pourquoi', 'pourquoi?', 'pk', 'pk?']
 
@@ -9,7 +10,8 @@ export default new Listener(
 		once: false
 	},
 	{
-		async run(message: Message) {
+		async run(message: Message, client: BotClient) {
+			if (client.config.environment !== 'production') return
 			if (message.guild.id !== '860721584373497887') return
 			let feur = false
 			for (const trigger of triggers) {
