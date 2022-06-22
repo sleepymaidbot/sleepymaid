97254062296a196f1959df9987a9ebc77861f7e5
Antoine <antoine.turgeon2005@hotmail.com>
2022-06-22 15:31:09 -0400

    style(LaserWelcome): remove unused client var

--- services/bot/src/listeners/laser/welcome.ts
+++ services/bot/src/listeners/laser/welcome.ts
@@ -6,7 +6,7 @@ export default new Listener(
 		once: false
 	},
 	{
-		async run(member, _client) {
+		async run(member) {
 			if (member.guild.id !== '860721584373497887') return
 			const role = await member.guild.roles.cache.get('872029952046927922')
 			await member.roles.add(role)
