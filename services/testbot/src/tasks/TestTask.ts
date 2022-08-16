6df240bc3fd14899af7f023f7bf081e66e340484
Antoine <antoine.turgeon2005@hotmail.com>
2022-08-16 00:50:15 -0400

    test: use node-cron

--- services/testbot/src/tasks/TestTask.ts
+++ services/testbot/src/tasks/TestTask.ts
@@ -3,7 +3,7 @@ import { injectable } from 'tsyringe'
 
 @injectable()
 export default class TestTask implements TaskInterface {
-	public readonly interval = 100000
+	public readonly interval = '* * * * *'
 	public execute(client: HandlerClient) {
 		client.logger.info('Task ran')
 	}
