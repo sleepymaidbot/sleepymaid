b7b43ffda4705a277f6e52b63699b1cb5f3be2e0
Antoine <antoine.turgeon2005@hotmail.com>
2022-08-18 18:54:06 -0400

    feat: add rainbow command

---
+++ services/helper/src/commands/chat/rainbow.ts
@@ -0,0 +1,70 @@
+import { SlashCommandInterface } from '@sleepymaid/handler'
+import {
+	ChatInputCommandInteraction,
+	resolveColor,
+	ColorResolvable
+} from 'discord.js'
+
+let onCooldown = false
+
+export default class RainbowCommand implements SlashCommandInterface {
+	public readonly guildIds = ['324284116021542922']
+	public readonly data = {
+		name: 'rainbow',
+		description: 'Change la couleur du rôle vraiment cool.'
+	}
+	public async execute(interaction: ChatInputCommandInteraction<'cached'>) {
+		const role = interaction.guild.roles.cache.get('944706938946609232')
+		if (!interaction.member.roles.cache.has('944706938946609232'))
+			return interaction.reply({
+				embeds: [
+					{
+						color: role.color,
+						description:
+							'<:redX:948606748334358559> Tu doit avoir le rôle `Vraiment Cool` pour utiliser cette commande.'
+					}
+				],
+				ephemeral: true
+			})
+
+		const getRandomColor = () => {
+			const letters = '0123456789ABCDEF'
+			let color = '#'
+			for (let i = 0; i < 6; i++) {
+				color += letters[Math.floor(Math.random() * 16)]
+			}
+			return color
+		}
+
+		if (onCooldown === true)
+			return interaction.reply({
+				embeds: [
+					{
+						color: role.color,
+						description:
+							'<:redX:948606748334358559> La commande est en cooldown.'
+					}
+				],
+				ephemeral: true
+			})
+
+		const color = resolveColor(getRandomColor() as ColorResolvable)
+
+		await role
+			.setColor(color, 'Changed by: ' + interaction.user.tag)
+			.then(() => {
+				onCooldown = true
+				setTimeout(() => (onCooldown = false), 300000)
+			})
+
+		return await interaction.reply({
+			embeds: [
+				{
+					color: color,
+					description: `<:greenTick:948620600144982026> La couleur du rôle vraiment cool a été changée en #${color}.`
+				}
+			],
+			ephemeral: true
+		})
+	}
+}
