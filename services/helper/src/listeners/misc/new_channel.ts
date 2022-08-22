4b75e0b99a696a9630ad95a4d1b0895419fbdf58
Antoine <antoine.turgeon2005@hotmail.com>
2022-08-21 21:43:12 -0400

    feat(new_channel): log to logger

--- services/helper/src/listeners/misc/new_channel.ts
+++ services/helper/src/listeners/misc/new_channel.ts
@@ -1,18 +1,19 @@
 import { ListenerInterface } from '@sleepymaid/handler'
 import { ChannelType } from 'discord-api-types/v10'
 import { Message } from 'discord.js'
+import { HelperClient } from '../../lib/extensions/HelperClient'
 
 export default class AutoPublishListener implements ListenerInterface {
 	public readonly name = 'messageCreate'
 	public readonly once = false
 
-	public async execute(message: Message) {
+	public async execute(message: Message, client: HelperClient) {
 		if (
 			message.channel.type === ChannelType.GuildNews &&
 			message.channel.id === '962902965914570812' &&
 			message.webhookId === '970352678993530940'
 		) {
-			message.crosspost().catch(console.error)
+			message.crosspost().catch(client.logger.error)
 		}
 	}
 }
