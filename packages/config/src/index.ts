f818f80a8570c483ba987835547cad8e8deb9ff9
Antoine <antoine.turgeon2005@hotmail.com>
2022-09-12 21:51:56 -0400

    feat(Config): rewrite to use env vars

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,1 +1,23 @@
-export * from './Config'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+export type NodeEnv = 'dev' | 'prod'
+export interface Config {
+	discordClientId: string
+	discordClientSecret: string
+	discordToken: string
+	devIds: string[]
+	dbUrl: string
+	nodeEnv: NodeEnv
+}
+
+export const initConfig = () => {
+	const config: Config = {
+		discordClientId: process.env.DISCORD_CLIENT_ID!,
+		devIds: process.env.DEV_IDS?.split(',') ?? [],
+		discordClientSecret: process.env.DISCORD_CLIENT_SECRET!,
+		discordToken: process.env.DISCORD_TOKEN!,
+		dbUrl: process.env.DB_URL!,
+		nodeEnv: (process.env.NODE_ENV as NodeEnv) ?? 'dev'
+	}
+	return config
+}
