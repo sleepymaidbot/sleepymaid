a41790236bc0c0967bbd19b4e93a575a435088c3
Antoine <antoine.turgeon2005@hotmail.com>
2022-10-08 22:43:20 -0400

    feat(Util): add tests

---
+++ packages/util/__tests__/prettyBytes.test.ts
@@ -0,0 +1,19 @@
+import { prettyBytes } from '../src/prettyBytes';
+import { describe, test, expect } from 'vitest';
+
+describe('prettyBytes', () => {
+	test('GIVEN a number with binary option as true THEN returns a string ', () => {
+		expect(
+			prettyBytes(1000, {
+				binary: true,
+			}),
+		).toBe('1000 B');
+		expect(prettyBytes(1024, { binary: true })).toBe('1 kiB');
+	});
+	test('GIVEN a number with bits option as true THEN returns a string ', () => {
+		expect(prettyBytes(1337, { bits: true })).toBe('1.34 kbit');
+	});
+	test('GIVEN a number with minimumFractionDigits option as 3 THEN returns a string ', () => {
+		expect(prettyBytes(1900, { minimumFractionDigits: 3 })).toBe('1.900 kB');
+	});
+});
