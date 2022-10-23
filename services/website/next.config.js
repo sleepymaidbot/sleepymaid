35f0d3030af5e25de1a553a3cd6ca7368aa5e99b
Antoine <antoine.turgeon2005@hotmail.com>
2022-10-23 16:13:45 -0400

    feat(website): rework next config

--- services/website/next.config.js
+++ services/website/next.config.js
@@ -1,10 +1,38 @@
 /* eslint-disable */
 // @ts-check
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	distDir: 'dist',
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
 	reactStrictMode: true,
+	swcMinify: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	productionBrowserSourceMaps: true,
+	cleanDistDir: true,
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
+	redirects() {
+		return [
+			{
+				source: '/github',
+				destination: 'https://github.com/sleepymaidbot/sleepymaid',
+				permanent: true,
+			},
+			{
+				source: '/support',
+				destination: 'https://discord.gg/8bpy2PC',
+				permanent: true,
+			},
+			{
+				source: '/invite',
+				destination: 'yo',
+				permanent: true,
+			},
+		];
+	},
 };
-
-module.exports = nextConfig;
