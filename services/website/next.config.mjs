59a10dcb0f7cea12aa64e79df39b89bab301399c
Ecorte <antoine.turgeon2005@hotmail.com>
2023-01-13 08:12:38 -0500

    chore: adapt website to monorepo

--- services/website/next.config.mjs
+++ services/website/next.config.mjs
@@ -3,7 +3,6 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -13,5 +12,6 @@ const config = {
 		locales: ['en'],
 		defaultLocale: 'en',
 	},
+	output: 'standalone',
 };
 export default config;
