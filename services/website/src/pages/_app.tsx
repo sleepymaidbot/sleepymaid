d83d024b02919fc3f97f9ab0b9e42ab1f0319130
Ecorte <antoine.turgeon2005@hotmail.com>
2023-01-12 20:56:22 -0500

    chore(website): use t3

--- services/website/src/pages/_app.tsx
+++ services/website/src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+import { api } from '../utils/api';
 
-export default MyApp;
+import '../styles/globals.css';
+
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
+};
+
+export default api.withTRPC(MyApp);
