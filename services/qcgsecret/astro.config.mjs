f9d602ebd977260ab755886f76a10674609dfbfd
Ecorte <antoine.turgeon2005@hotmail.com>
2023-03-10 11:35:46 -0500

    refactor(AstroConfig): remove useless comment

--- services/qcgsecret/astro.config.mjs
+++ services/qcgsecret/astro.config.mjs
@@ -11,42 +11,7 @@ const rootDir = new URL('../../', import.meta.url);
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react(),
-		mdx(/*{
-			rehypePlugins: [
-				rehypeSlug,
-				[
-					rehypeAutolinkHeadings,
-					{
-						properties: {
-							class:
-								'relative inline-flex w-6 h-6 place-items-center place-content-center outline-0 text-black dark:text-white ml-2',
-						},
-						behavior: 'after',
-						group: ({ tagName }: { tagName: string }) =>
-							h('div', {
-								class: `[&>*]:inline-block [&>h1]:m-0 [&>h2]:m-0 [&>h3]:m-0 [&>h4]:m-0 level-${tagName}`,
-								tabIndex: -1,
-							}),
-						content: (heading: Node) => [
-							h(
-								`span.anchor-icon`,
-								{
-									ariaHidden: 'true',
-								},
-								LinkIcon,
-							),
-							createSROnlyLabel(toString(heading)),
-						],
-					},
-				],
-			],
-		}*/),
-		compress(),
-		critters(),
-		tailwind(),
-	],
+	integrations: [react(), mdx(), compress(), critters(), tailwind()],
 	output: 'server',
 	adapter: node({
 		mode: 'standalone',
