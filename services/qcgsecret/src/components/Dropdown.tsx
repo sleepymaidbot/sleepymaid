19812b82d06f4294705fbd75434c9f59faca4f11
Ecorte <antoine.turgeon2005@hotmail.com>
2023-07-11 14:29:43 -0400

    feat(qcgsecret): update ariakit

--- services/qcgsecret/src/components/Dropdown.tsx
+++ services/qcgsecret/src/components/Dropdown.tsx
@@ -1,7 +1,7 @@
 import '../styles/Dropdown.css';
 import { BsArrowDownLeft } from '@react-icons/all-files/bs/BsArrowDownLeft';
 import { BsArrowUpLeft } from '@react-icons/all-files/bs/BsArrowUpLeft';
-import { Disclosure, DisclosureContent, useDisclosureState } from 'ariakit/disclosure';
+import { Disclosure, DisclosureContent, useDisclosureStore } from '@ariakit/react';
 import type { PropsWithChildren } from 'react';
 
 export type IDropdown = {
@@ -9,8 +9,8 @@ export type IDropdown = {
 };
 
 export default function Dropdown({ title, children }: PropsWithChildren<IDropdown>) {
-	const disclosure = useDisclosureState();
-	const icon = disclosure.open ? (
+	const disclosure = useDisclosureStore();
+	const icon = disclosure.getState().open ? (
 		<BsArrowUpLeft color="white" size={20} />
 	) : (
 		<BsArrowDownLeft color="white" size={20} />
@@ -19,13 +19,13 @@ export default function Dropdown({ title, children }: PropsWithChildren<IDropdow
 	return (
 		<div className="ext">
 			<div className="wrapper">
-				<Disclosure className="button" state={disclosure}>
+				<Disclosure className="button" store={disclosure}>
 					<div className="button-container">
 						<span className="button-text">{title}</span>
 						<span className="icon">{icon}</span>
 					</div>
 				</Disclosure>
-				<DisclosureContent className="content" state={disclosure}>
+				<DisclosureContent className="content" store={disclosure}>
 					<p>{children}</p>
 				</DisclosureContent>
 			</div>
