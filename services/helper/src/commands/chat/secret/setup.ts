94f46240c6f766c4e60ae497774ecf12d02649a0
Ecorte <antoine.turgeon2005@hotmail.com>
2023-07-21 14:51:35 -0400

    refactor(QCGSSetup): add catch

--- services/helper/src/commands/chat/secret/setup.ts
+++ services/helper/src/commands/chat/secret/setup.ts
@@ -99,55 +99,66 @@ export default class QCGSecretSetupCommand implements SlashCommandInterface {
 		if (messageId) {
 			const message = await interaction.channel?.messages.fetch(messageId);
 			if (!message) {
-				await interaction.reply({
-					embeds: [
-						{
-							color: 3553599,
-							description: '<:redX:948606748334358559> Message not found.',
-						},
-					],
-					ephemeral: true,
-				});
+				await interaction
+					.reply({
+						embeds: [
+							{
+								color: 3553599,
+								description: '<:redX:948606748334358559> Message not found.',
+							},
+						],
+						ephemeral: true,
+					})
+					.catch(client.logger.error);
 			}
 			if (message?.author.id !== client.user?.id) {
-				await interaction.reply({
-					embeds: [
-						{
-							color: 3553599,
-							description: '<:redX:948606748334358559> You can only edit messages sent by the bot.',
-						},
-					],
-					ephemeral: true,
-				});
+				await interaction
+					.reply({
+						embeds: [
+							{
+								color: 3553599,
+								description: '<:redX:948606748334358559> You can only edit messages sent by the bot.',
+							},
+						],
+						ephemeral: true,
+					})
+					.catch(client.logger.error);
 			} else {
 				const msgs = await msg.function(interaction);
 				if (msgs.length === 1) await message?.edit((await msgs[0]) as MessageEditOptions);
 				else
-					await interaction.reply({
-						embeds: [
-							{
-								color: 3553599,
-								description: '<:redX:948606748334358559> Message too big.',
-							},
-						],
-						ephemeral: true,
-					});
+					await interaction
+						.reply({
+							embeds: [
+								{
+									color: 3553599,
+									description: '<:redX:948606748334358559> Message too big.',
+								},
+							],
+							ephemeral: true,
+						})
+						.catch(client.logger.error);
 			}
 		} else {
-			await msg.function(interaction).then((msgs) =>
-				msgs.forEach(async (msg) => {
-					await interaction?.channel?.send({ ...msg, allowedMentions: { parse: [] } });
-				}),
-			);
+			await msg
+				.function(interaction)
+				.then((msgs) =>
+					msgs.forEach(async (msg) => {
+						await interaction?.channel?.send({ ...msg, allowedMentions: { parse: [] } }).catch(client.logger.error);
+					}),
+				)
+				.catch(client.logger.error);
 		}
-		await interaction.reply({
-			embeds: [
-				{
-					color: 3553599,
-					description: '<:greenTick:948620600144982026> Done!',
-				},
-			],
-			ephemeral: true,
-		});
+		await interaction
+			.reply({
+				embeds: [
+					{
+						color: 3553599,
+						description: '<:greenTick:948620600144982026> Done!',
+					},
+				],
+				ephemeral: true,
+			})
+			.catch(client.logger.error);
 	}
 }
