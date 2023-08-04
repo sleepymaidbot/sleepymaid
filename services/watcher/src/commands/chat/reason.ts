728ca8a6f5a737883f7a63b89127e36fc9664a50
Ecorte <antoine.turgeon2005@hotmail.com>
2023-08-04 01:15:29 -0400

    feat(ReasonCommand): add options

--- services/watcher/src/commands/chat/reason.ts
+++ services/watcher/src/commands/chat/reason.ts
@@ -1,13 +1,33 @@
 import type { SlashCommandInterface } from '@sleepymaid/handler';
 import { getLocalizedProp } from '@sleepymaid/shared';
-import { ChatInputCommandInteraction, PermissionFlagsBits, PermissionsBitField } from 'discord.js';
+import {
+	ApplicationCommandOptionType,
+	ChatInputApplicationCommandData,
+	ChatInputCommandInteraction,
+	PermissionFlagsBits,
+	PermissionsBitField,
+} from 'discord.js';
 
 export default class ReasonCommand implements SlashCommandInterface {
 	public readonly data = {
 		...getLocalizedProp('name', 'commands.reason.name'),
 		...getLocalizedProp('description', 'commands.reason.description'),
 		defaultMemberPermissions: new PermissionsBitField([PermissionFlagsBits.ModerateMembers]),
-	};
+		options: [
+			{
+				...getLocalizedProp('name', 'commands.reason.caseNumber.name'),
+				...getLocalizedProp('description', 'commands.reason.caseNumber.description'),
+				type: ApplicationCommandOptionType.Integer,
+				required: true,
+			},
+			{
+				...getLocalizedProp('name', 'commands.reason.reason.name'),
+				...getLocalizedProp('description', 'commands.reason.reason.description'),
+				type: ApplicationCommandOptionType.String,
+				required: true,
+			},
+		],
+	} as ChatInputApplicationCommandData;
 	public async execute(interaction: ChatInputCommandInteraction<'cached'>) {
 		interaction.reply({ content: 'This command is not yet implemented', ephemeral: true });
 	}
