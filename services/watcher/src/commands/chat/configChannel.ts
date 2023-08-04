78ed015ce8ece186efe11b1ed337075e6ba65749
Ecorte <antoine.turgeon2005@hotmail.com>
2023-08-04 16:28:30 -0400

    feat(ConfigCmd): add subscribed logs config

--- services/watcher/src/commands/chat/configChannel.ts
+++ services/watcher/src/commands/chat/configChannel.ts
@@ -1,5 +1,6 @@
 import type { SlashCommandInterface } from '@sleepymaid/handler';
 import {
+	APISelectMenuOption,
 	ApplicationCommandOptionType,
 	ButtonStyle,
 	ChannelType,
@@ -12,6 +13,113 @@ import {
 } from 'discord.js';
 import { WatcherClient } from '../../lib/extensions/WatcherClient';
 import { LogChannelType } from '@prisma/client';
+
+export const subscribedLogsSelectOptions = {
+	mod: [
+		{
+			label: 'Timeout',
+			value: 'timeout',
+		},
+		{
+			label: 'Untimeout',
+			value: 'untimeout',
+		},
+		{
+			label: 'Kick',
+			value: 'kick',
+		},
+		{
+			label: 'Ban',
+			value: 'ban',
+		},
+		{
+			label: 'Unban',
+			value: 'unban',
+		},
+	],
+	server: [
+		{
+			label: 'Message Edit',
+			value: 'messageEdit',
+		},
+		{
+			label: 'Message Delete',
+			value: 'messageDelete',
+		},
+		{
+			label: 'Member Join',
+			value: 'memberJoin',
+		},
+		{
+			label: 'Member Leave',
+			value: 'memberLeave',
+		},
+		{
+			label: 'Member Nickname Update',
+			value: 'memberNicknameUpdate',
+		},
+		{
+			label: 'Member Role Update',
+			value: 'memberRoleUpdate',
+		},
+		{
+			label: 'Member Voice Update',
+			value: 'memberVoiceUpdate',
+		},
+		{
+			label: 'Member Avatar Update',
+			value: 'memberAvatarUpdate',
+		},
+		{
+			label: 'Member Username Update',
+			value: 'memberUsernameUpdate',
+		},
+		{
+			label: 'Role Create',
+			value: 'roleCreate',
+		},
+		{
+			label: 'Role Delete',
+			value: 'roleDelete',
+		},
+		{
+			label: 'Role Update',
+			value: 'roleUpdate',
+		},
+		{
+			label: 'Channel Create',
+			value: 'channelCreate',
+		},
+		{
+			label: 'Channel Delete',
+			value: 'channelDelete',
+		},
+		{
+			label: 'Channel Update',
+			value: 'channelUpdate',
+		},
+		{
+			label: 'Emoji Create',
+			value: 'emojiCreate',
+		},
+		{
+			label: 'Emoji Delete',
+			value: 'emojiDelete',
+		},
+		{
+			label: 'Emoji Update',
+			value: 'emojiUpdate',
+		},
+		{
+			label: 'Invite Create',
+			value: 'inviteCreate',
+		},
+		{
+			label: 'Invite Delete',
+			value: 'inviteDelete',
+		},
+	],
+};
 
 export default class ConfigCommand implements SlashCommandInterface {
 	public readonly data = {
@@ -45,7 +153,113 @@ export default class ConfigCommand implements SlashCommandInterface {
 		});
 
 		if (channelConfig) {
-			// TODO: Add a way to configure the subscribed logs.
+			const options = (): APISelectMenuOption[] => {
+				const o: APISelectMenuOption[] = [];
+				for (const values of subscribedLogsSelectOptions[channelConfig.type]) {
+					o.push({ ...values, default: channelConfig[values.value as keyof typeof channelConfig] as boolean });
+				}
+				return o;
+			};
+
+			await interaction.reply({
+				content: 'Please select which logs you want to subscribe to.',
+				ephemeral: true,
+				components: [
+					{
+						type: ComponentType.ActionRow,
+						components: [
+							{
+								type: ComponentType.StringSelect,
+								placeholder: 'Select a log type',
+								custom_id: 'configChannel:config:subSelect',
+								max_values: 1,
+								min_values: 1,
+								options: options(),
+							},
+						],
+					},
+					{
+						type: ComponentType.ActionRow,
+						components: [
+							{
+								type: ComponentType.Button,
+								label: 'Remove logs from this channel',
+								style: ButtonStyle.Danger,
+								customId: 'configChannel:config:removeLogs',
+								emoji: {
+									id: '948606748334358559',
+								},
+							},
+						],
+					},
+				],
+			});
+
+			await interaction.channel
+				.awaitMessageComponent({
+					time: 15_0000,
+					filter: (i) => i.user.id === interaction.user.id && i.customId.startsWith('configChannel:config:'),
+				})
+				.then(async (i) => {
+					await i.deferUpdate();
+					switch (i.customId) {
+						case 'configChannel:config:subSelect': {
+							if (!i.isStringSelectMenu()) return;
+							const selected = i.values;
+							const typeValue = subscribedLogsSelectOptions[channelConfig.type].map((v) => v.value);
+							const transaction = [];
+							for (const v of typeValue) {
+								if (!selected.includes(v)) {
+									transaction.push(
+										client.prisma.logChannel.update({
+											where: {
+												guildId: interaction.guild.id,
+												channelId: channel.id,
+											},
+											data: {
+												[v]: false,
+											},
+										}),
+									);
+								} else {
+									transaction.push(
+										client.prisma.logChannel.update({
+											where: {
+												guildId: interaction.guild.id,
+												channelId: channel.id,
+											},
+											data: {
+												[v]: true,
+											},
+										}),
+									);
+								}
+							}
+							await client.prisma.$transaction(transaction);
+							break;
+						}
+						case 'configChannel:config:removeLogs': {
+							await client.prisma.logChannel.delete({
+								where: {
+									guildId: interaction.guild.id,
+									channelId: channel.id,
+								},
+							});
+							await i.update({
+								content: 'Successfully removed logs from this channel.',
+								components: [],
+							});
+							break;
+						}
+					}
+				})
+				.catch(client.logger.error)
+				.finally(async () => {
+					await interaction.editReply({
+						content: 'This message is now inactive.',
+						components: [],
+					});
+				});
 		} else {
 			await interaction.reply({
 				content: 'This channel is not yet configured. Do you want to configure it?',
