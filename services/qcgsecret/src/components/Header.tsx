6230281914c5c756e249b400cfd967356e98c39a
Ecorte <antoine.turgeon2005@hotmail.com>
2023-08-07 21:51:58 -0400

    feat(HeaderComponent): add link next to title

--- services/qcgsecret/src/components/Header.tsx
+++ services/qcgsecret/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react';
+import { BsLink45Deg } from '@react-icons/all-files/bs/BsLink45Deg';
 import '../styles/Header.css';
 
 export type IHeader = {
@@ -7,9 +8,20 @@ export type IHeader = {
 };
 
 export default function Header({ title, id, children }: PropsWithChildren<IHeader>) {
+	const url = window.location.pathname.split('#')[0] + '#' + id;
 	return (
 		<div id={id} className="header-component">
-			<h1>{title}</h1>
+			<div className="component-aligned">
+				<span>
+					<h1>{title}</h1>
+				</span>
+				<span className="component-icon">
+					{' '}
+					<a className="anchor-icon" href={url}>
+						<BsLink45Deg size={20}></BsLink45Deg>
+					</a>
+				</span>
+			</div>
 			<p>{children}</p>
 		</div>
 	);
