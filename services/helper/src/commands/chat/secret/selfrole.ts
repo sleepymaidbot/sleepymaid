6a9cb70c9bae205608fe8da965fc6835f7b0577e
Ecorte <antoine.turgeon2005@hotmail.com>
2023-08-19 00:05:09 -0400

    feat(Helper): add QCGSecret quick selfrole

---
+++ services/helper/src/commands/chat/secret/selfrole.ts
@@ -0,0 +1,93 @@
+import { SlashCommandInterface } from '@sleepymaid/handler';
+import {
+	ChatInputCommandInteraction,
+	ApplicationCommandOptionType,
+	ApplicationCommandType,
+	ChatInputApplicationCommandData,
+	PermissionFlagsBits,
+	PermissionsBitField,
+} from 'discord.js';
+import type { HelperClient } from '../../../lib/extensions/HelperClient';
+import { ButtonStyle } from 'discord-api-types/v10';
+
+export default class QCGSecretSetupCommand implements SlashCommandInterface {
+	public readonly guildIds = ['1131653884377579651'];
+	public readonly data = {
+		name: 'qcgsselfrole',
+		description: '[Admin only] Allow you to post a simple self role message.',
+		type: ApplicationCommandType.ChatInput,
+		defaultMemberPermissions: new PermissionsBitField([PermissionFlagsBits.Administrator]),
+		options: [
+			{
+				name: 'role',
+				description: 'The role',
+				type: ApplicationCommandOptionType.Role,
+				required: true,
+			},
+		],
+	} as ChatInputApplicationCommandData;
+
+	// @ts-ignore
+	public async execute(interaction: ChatInputCommandInteraction, client: HelperClient) {
+		if (!interaction.inCachedGuild()) return;
+		const role = interaction.options.getRole('role');
+		if (!role) return;
+		if (
+			role.permissions.any([
+				PermissionFlagsBits.Administrator,
+				PermissionFlagsBits.BanMembers,
+				PermissionFlagsBits.KickMembers,
+				PermissionFlagsBits.ManageChannels,
+				PermissionFlagsBits.ManageEmojisAndStickers,
+				PermissionFlagsBits.ManageGuild,
+				PermissionFlagsBits.ManageMessages,
+				PermissionFlagsBits.ManageRoles,
+				PermissionFlagsBits.ManageWebhooks,
+			])
+		)
+			return interaction.reply({
+				content: 'You cannot use this command on an admin role.',
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (!client.user) return;
+		if (!interaction.guild) return;
+		const bot = interaction.guild.members.cache.get(client.user.id);
+		if (!bot) return;
+		if (role.position >= bot.roles.highest.position)
+			return interaction.reply({
+				content: 'You cannot use this command on a role higher than the bot.',
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (role.position >= interaction.member.roles.highest.position && interaction.user.id !== interaction.guild.ownerId)
+			return interaction.reply({
+				content: 'You cannot use this command on a role higher than yours.',
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (!interaction.channel) return;
+		interaction.reply({
+			content: 'Self role message created!',
+			ephemeral: true,
+			allowedMentions: { parse: [] },
+		});
+		interaction.channel.send({
+			content: 'Click the button below to get the ' + role.toString() + ' role!',
+			allowedMentions: { parse: [] },
+			components: [
+				{
+					type: 1,
+					components: [
+						{
+							type: 2,
+							style: ButtonStyle.Primary,
+							label: 'Add/Remove the role',
+							custom_id: 'qcgselfrole:' + role.id,
+						},
+					],
+				},
+			],
+		});
+	}
+}
---
+++ services/helper/src/listeners/qcgSecretInteractions/selfroles.ts
@@ -0,0 +1,33 @@
+import type { ListenerInterface } from '@sleepymaid/handler';
+import { BaseInteraction } from 'discord.js';
+
+export default class DebugListener implements ListenerInterface {
+	public readonly name = 'interactionCreate';
+	public readonly once = false;
+
+	public async execute(interaction: BaseInteraction) {
+		if (!interaction.isButton()) return;
+		if (!interaction.inCachedGuild()) return;
+		if (!interaction.customId.startsWith('qcgselfrole:')) return;
+		const roleId = interaction.customId.split(':')[1];
+		if (!roleId) return;
+		const role = interaction.guild?.roles.cache.get(roleId);
+		if (!role) return;
+		if (!interaction.member) return;
+		if (interaction.member.roles.cache.has(roleId)) {
+			await interaction.member.roles.remove(roleId);
+			await interaction.reply({
+				content: `You no longer have the role ${role.toString()}`,
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		} else {
+			await interaction.member.roles.add(roleId);
+			await interaction.reply({
+				content: `You now have the role ${role.toString()}`,
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		}
+	}
+}
