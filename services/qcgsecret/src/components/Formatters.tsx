84e78379aadd960f234916dd480ffcd71abf7d31
Ecorte <antoine.turgeon2005@hotmail.com>
2023-08-30 13:11:05 -0400

    refactor(Formatters): use tailwindcss

--- services/qcgsecret/src/components/Formatters.tsx
+++ services/qcgsecret/src/components/Formatters.tsx
@@ -1,5 +1,4 @@
 import type { PropsWithChildren } from 'react';
-import '../styles/Formatters.css';
 
 export function Underline({ children }: PropsWithChildren<Record<string, never>>) {
 	return <span className="underline">{children}</span>;
