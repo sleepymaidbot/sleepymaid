a74480ba9da4ab232e87040aee6bb352e5c85e30
Ecorte <antoine.turgeon2005@hotmail.com>
2023-09-29 13:56:39 -0400

    feat(Whiteout): add autorole

---
+++ services/helper/src/listeners/whiteout/welcome.ts
@@ -0,0 +1,14 @@
+import type { ListenerInterface } from '@sleepymaid/handler';
+import type { GuildMember } from 'discord.js';
+
+export default class WelcomeListener implements ListenerInterface {
+	public readonly name = 'guildMemberAdd';
+	public readonly once = false;
+
+	public async execute(member: GuildMember) {
+		if (member.guild.id !== '1150379660128047104') return;
+		const role = member.guild.roles.cache.get('1150379660128047104');
+		if (!role) return;
+		await member.roles.add(role);
+	}
+}
