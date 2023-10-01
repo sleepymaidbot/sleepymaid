6e69bf9f5775a66ae2a8a5e8ef3e1066b28820e9
Ecorte <antoine.turgeon2005@hotmail.com>
2023-10-01 09:15:20 -0400

    feat(WhiteoutAutoRole): remove bots

--- services/helper/src/listeners/whiteout/welcome.ts
+++ services/helper/src/listeners/whiteout/welcome.ts
@@ -7,6 +7,7 @@ export default class WelcomeListener implements ListenerInterface {
 
 	public async execute(member: GuildMember) {
 		if (member.guild.id !== '1150379660128047104') return;
+		if (member.user.bot) return;
 		const role = member.guild.roles.cache.get('1150541368498856017');
 		if (!role) return;
 		await member.roles.add(role);
