c6a71788869de0f012d25cf9c4afff1f7353fc72
Ecorte <antoine.turgeon2005@hotmail.com>
2023-11-27 19:14:09 -0500

    feat(Helper): add channel sanitizer

---
+++ services/helper/src/listeners/whiteout/chanSan.ts
@@ -0,0 +1,14 @@
+import type { ListenerInterface } from '@sleepymaid/handler';
+import sanitize from '@aero/sanitizer';
+import { GuildChannel } from 'discord.js';
+
+export default class WelcomeListener implements ListenerInterface {
+	public readonly name = 'channelUpdate';
+	public readonly once = true;
+
+	public async execute(oldChannel: GuildChannel, newChannel: GuildChannel) {
+		if (oldChannel.guild.id !== '1150379660128047104') return;
+		const sanitized = sanitize(newChannel.name);
+		if (newChannel.name !== sanitized) await newChannel.setName(sanitized, 'Sanitizer');
+	}
+}
