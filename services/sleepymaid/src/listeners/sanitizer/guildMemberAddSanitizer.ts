cb2af7ca27173f2e240315074c83898a8280e58e
Ecorte <antoine.turgeon2005@hotmail.com>
2023-12-04 19:10:00 -0500

    fix(sanitizer): check if guild has sanitizer enabled

--- services/sleepymaid/src/listeners/sanitizer/guildMemberAddSanitizer.ts
+++ services/sleepymaid/src/listeners/sanitizer/guildMemberAddSanitizer.ts
@@ -1,13 +1,18 @@
 import type { ListenerInterface } from '@sleepymaid/handler';
 import type { GuildMember } from 'discord.js';
 import sanitize from '@aero/sanitizer';
+import { SleepyMaidClient } from '../../lib/extensions/SleepyMaidClient';
 
 export default class JoinSanitizerListener implements ListenerInterface {
 	public readonly name = 'guildMemberAdd';
 	public readonly once = false;
 
-	public async execute(member: GuildMember) {
+	public async execute(member: GuildMember, client: SleepyMaidClient) {
 		const sanitized = sanitize(member.displayName);
+		const sanitizerSettings = await client.prisma.sanitizerSettings.findUnique({
+			where: { guildId: member.guild.id },
+		});
+		if (sanitizerSettings?.enabled === false) return;
 		if (member.displayName !== sanitized) await member.setNickname(sanitized, 'Sanitizer');
 	}
 }
