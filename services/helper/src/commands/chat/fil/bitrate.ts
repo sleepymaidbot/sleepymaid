b07c2ce87befb0263f391568801e8336d321d8a9
Ecorte <antoine.turgeon2005@hotmail.com>
2024-03-31 17:43:53 -0400

    feat(randomBitrate): add random Bitrate toggle

--- services/helper/src/commands/chat/fil/bitrate.ts
+++ services/helper/src/commands/chat/fil/bitrate.ts
@@ -1,27 +1,94 @@
 import type { SlashCommandInterface } from '@sleepymaid/handler';
 import { ApplicationCommandType } from 'discord-api-types/v10';
-import { ChatInputCommandInteraction, ChatInputApplicationCommandData } from 'discord.js';
+import type { ChatInputCommandInteraction, ChatInputApplicationCommandData } from 'discord.js';
+import { ApplicationCommandOptionType, ChannelType } from 'discord.js';
 import type { HelperClient } from '../../../lib/extensions/HelperClient';
+import { and, eq } from 'drizzle-orm';
+import { randomBitrate } from '@sleepymaid/db';
 
 export default class RandomBitrateCommand implements SlashCommandInterface {
-	public readonly guildIds = ['796534493535928320'];
+	public readonly guildIds = ['796534493535928320', '821717486217986098'];
+
 	public readonly data = {
 		name: 'randombitrate',
 		description: 'Random bitrate for the voice channel.',
 		type: ApplicationCommandType.ChatInput,
+		options: [
+			{
+				name: 'now',
+				description: 'Randomize the bitrate now.',
+				type: ApplicationCommandOptionType.Subcommand,
+			},
+			{
+				name: 'toggle',
+				description: 'Toggle the random bitrate in a channel.',
+				type: ApplicationCommandOptionType.Subcommand,
+				options: [
+					{
+						name: 'channel',
+						description: 'The channel to toggle the random bitrate.',
+						type: ApplicationCommandOptionType.Channel,
+						required: true,
+					},
+					{
+						name: 'state',
+						description: 'The state of the random bitrate.',
+						type: ApplicationCommandOptionType.Boolean,
+						required: true,
+					},
+				],
+			},
+		],
 	} as ChatInputApplicationCommandData;
 
-	// @ts-ignore
+	// @ts-expect-error client overriden
 	public async execute(interaction: ChatInputCommandInteraction, client: HelperClient) {
 		if (!interaction.inCachedGuild()) return;
 		if (!interaction.guild) return;
-		if (!interaction.member.voice.channel)
-			return interaction.reply('You need to be in a voice channel to use this command.');
-		const maxBitrate = interaction.guild.maximumBitrate;
-		const minBitrate = 8000;
-		// Random bitrate between min and maxBitrate
-		const bitrate = Math.floor(Math.random() * (maxBitrate - minBitrate + 1) + minBitrate);
-		await interaction.member.voice.channel.setBitrate(bitrate);
-		await interaction.reply(`The bitrate for the voice channel has been set to ${bitrate}kbps.`);
+		if (interaction.options.getSubcommand() === 'now') {
+			if (!interaction.member.voice.channel)
+				return interaction.reply('You need to be in a voice channel to use this command.');
+			const maxBitrate = interaction.guild.maximumBitrate;
+			const minBitrate = 8_000;
+			// Random bitrate between min and maxBitrate
+			const bitrate = Math.floor(Math.random() * (maxBitrate - minBitrate + 1) + minBitrate);
+			// Round up the birate by to get the last 3 digit to be 0
+			const roundedBitrate = Math.ceil(bitrate / 1_000) * 1_000;
+			await interaction.member.voice.channel.setBitrate(roundedBitrate);
+			await interaction.reply(`The bitrate for the voice channel has been set to ${Math.ceil(bitrate / 1_000)}kbps.`);
+		} else if (interaction.options.getSubcommand() === 'toggle') {
+			const target = interaction.options.getChannel('channel', true);
+			const state = interaction.options.getBoolean('state', true);
+			if (target.type !== ChannelType.GuildVoice) return interaction.reply('The channel must be a voice channel.');
+			if (target.members.size === 0) return interaction.reply('There are no members in the voice channel.');
+			const channelSettings = await client.drizzle.query.randomBitrate.findFirst({
+				where: and(eq(randomBitrate.guildId, interaction.guild.id), eq(randomBitrate.channelId, target.id)),
+			});
+			if (!channelSettings) {
+				if (state === true) {
+					await client.drizzle.insert(randomBitrate).values({
+						guildId: interaction.guild.id,
+						channelId: target.id,
+						enabled: true,
+					});
+					await interaction.reply(`The random bitrate has been enabled for ${target.toString()}.`);
+				} else {
+					await interaction.reply(`The random bitrate is already disabled for ${target.toString()}.`);
+				}
+			} else {
+				if (state === true) {
+					await client.drizzle
+						.update(randomBitrate)
+						.set({ enabled: true })
+						.where(and(eq(randomBitrate.guildId, interaction.guild.id), eq(randomBitrate.channelId, target.id)));
+					await interaction.reply(`The random bitrate has been enabled for ${target.toString()}.`);
+				} else {
+					await client.drizzle
+						.delete(randomBitrate)
+						.where(and(eq(randomBitrate.guildId, interaction.guild.id), eq(randomBitrate.channelId, target.id)));
+					await interaction.reply(`The random bitrate has been disabled for ${target.toString()}.`);
+				}
+			}
+		}
 	}
 }
