965b64f324f85ccdeffa6233849afd20986670b5
Ecorte <antoine.turgeon2005@hotmail.com>
2024-03-31 17:43:12 -0400

    fix(drizzle): type with schema

--- services/helper/src/lib/extensions/HelperClient.ts
+++ services/helper/src/lib/extensions/HelperClient.ts
@@ -1,23 +1,32 @@
+/* eslint-disable n/prefer-global/process */
+/* eslint-disable unicorn/prefer-module */
+import { resolve } from 'node:path';
+import process from 'node:process';
 import { PubSubRedisBroker } from '@discordjs/brokers';
 import { schema } from '@sleepymaid/db';
 import { HandlerClient } from '@sleepymaid/handler';
 import { Logger } from '@sleepymaid/logger';
-import { Config, initConfig, supportedLngs } from '@sleepymaid/shared';
+import type { Config } from '@sleepymaid/shared';
+import { initConfig, supportedLngs } from '@sleepymaid/shared';
 import { ActivityType, GatewayIntentBits } from 'discord-api-types/v10';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import i18next from 'i18next';
 import FsBackend from 'i18next-fs-backend';
 import Redis from 'ioredis';
-import { resolve } from 'path';
 import { Client } from 'pg';
 
 export class HelperClient extends HandlerClient {
 	public declare PGClient: Client;
-	public declare drizzle: ReturnType<typeof drizzle>;
+
+	public declare drizzle: ReturnType<typeof drizzle<typeof schema>>;
+
 	public declare redis: Redis;
+
 	public declare brokers: PubSubRedisBroker<Redis>;
+
 	public declare config: Config;
-	constructor() {
+
+	public constructor() {
 		super(
 			{
 				devServerId: '821717486217986098',
@@ -58,7 +67,7 @@ export class HelperClient extends HandlerClient {
 		this.drizzle = drizzle(this.PGClient, { schema });
 
 		await i18next.use(FsBackend).init({
-			//debug: this.config.environment === 'development',
+			// debug: this.config.environment === 'development',
 			supportedLngs,
 			backend: {
 				loadPath: resolve(__dirname, '../../../../../locales/sleepymaid/{{lng}}/{{ns}}.json'),
@@ -70,7 +79,7 @@ export class HelperClient extends HandlerClient {
 			ns: 'translation',
 		});
 
-		this.loadHandlers({
+		void this.loadHandlers({
 			commands: {
 				folder: resolve(__dirname, '..', '..', 'commands'),
 			},
@@ -82,7 +91,7 @@ export class HelperClient extends HandlerClient {
 			},
 		});
 
-		this.login(this.config.discordToken);
+		void this.login(this.config.discordToken);
 
 		process.on('unhandledRejection', (error: Error) => {
 			this.logger.error(error);
--- services/sleepymaid/src/lib/extensions/SleepyMaidClient.ts
+++ services/sleepymaid/src/lib/extensions/SleepyMaidClient.ts
@@ -1,23 +1,32 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable n/prefer-global/process */
+/* eslint-disable unicorn/prefer-module */
+import { resolve } from 'node:path';
 import { PubSubRedisBroker } from '@discordjs/brokers';
 import { schema } from '@sleepymaid/db';
 import { HandlerClient } from '@sleepymaid/handler';
 import { Logger } from '@sleepymaid/logger';
-import { Config, initConfig, supportedLngs } from '@sleepymaid/shared';
+import type { Config } from '@sleepymaid/shared';
+import { initConfig, supportedLngs } from '@sleepymaid/shared';
 import { ActivityType, GatewayIntentBits } from 'discord-api-types/v10';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import i18next from 'i18next';
 import FsBackend from 'i18next-fs-backend';
 import Redis from 'ioredis';
-import { resolve } from 'path';
 import { Client } from 'pg';
 
 export class SleepyMaidClient extends HandlerClient {
 	public declare PGClient: Client;
-	public declare drizzle: ReturnType<typeof drizzle>;
+
+	public declare drizzle: ReturnType<typeof drizzle<typeof schema>>;
+
 	public declare redis: Redis;
+
 	public declare brokers: PubSubRedisBroker<Redis>;
+
 	public declare config: Config;
-	constructor() {
+
+	public constructor() {
 		super(
 			{
 				devServerId: '821717486217986098',
@@ -59,7 +68,7 @@ export class SleepyMaidClient extends HandlerClient {
 		this.drizzle = drizzle(this.PGClient, { schema });
 
 		await i18next.use(FsBackend).init({
-			//debug: this.config.environment === 'development',
+			// debug: this.config.environment === 'development',
 			supportedLngs,
 			backend: {
 				loadPath: resolve(__dirname, '../../../../../locales/sleepymaid/{{lng}}/{{ns}}.json'),
@@ -82,7 +91,7 @@ export class SleepyMaidClient extends HandlerClient {
 				folder: resolve(__dirname, '..', '..', 'tasks'),
 			},
 		});
-		this.login(this.config.discordToken);
+		void this.login(this.config.discordToken);
 
 		process.on('unhandledRejection', (error: Error) => {
 			this.logger.error(error);
