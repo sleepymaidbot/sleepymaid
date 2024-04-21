cd6e743d0d05374cb58f668d2e203ac6536d161f
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-20 23:38:22 -0400

    feat(dashboard): add guild dashboard

---
+++ services/website/src/app/dashboard/[id]/page.tsx
@@ -0,0 +1,37 @@
+import { api } from "@/trpc/server";
+
+const Page = async ({ params }: { readonly params: { id: string } }) => {
+  const guildSettings = await api.guilds.getGuildSettings({
+    guildId: params.id,
+  });
+  const test = JSON.stringify(guildSettings);
+
+  return (
+    <div className="container flex items-center justify-center">
+      Guild id: {params.id}
+      <br />
+      {test}
+    </div>
+  );
+};
+
+export default Page;
+
+/* export default async function Page({
+  params,
+}: {
+  readonly params: { id: string };
+}) {
+  const guildSettings = await api.guilds.getGuildSettings({
+    guildId: params.id,
+  });
+  const test = JSON.stringify(guildSettings);
+
+  return (
+    <div className="container flex items-center justify-center">
+      Guild id: {params.id}
+      <br />
+      {test}
+    </div>
+  );
+}*/
