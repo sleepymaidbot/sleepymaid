4673aeb6c5afccb36dcd348e4a9141c0d85d47e2
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-20 23:37:17 -0400

    feat(ready): add guild name and icon setter

--- services/sleepymaid/src/listeners/client/ready.ts
+++ services/sleepymaid/src/listeners/client/ready.ts
@@ -1,10 +1,13 @@
+/* eslint-disable id-length */
+import { guildsSettings } from '@sleepymaid/db';
 import type { ListenerInterface } from '@sleepymaid/handler';
 import type { Guild } from 'discord.js';
+import { eq } from 'drizzle-orm';
 import type { SleepyMaidClient } from '../../lib/extensions/SleepyMaidClient';
-import { guildsSettings } from '@sleepymaid/db';
 
 export default class ReadyListener implements ListenerInterface {
 	public readonly name = 'ready';
+
 	public readonly once = true;
 
 	public async execute(client: SleepyMaidClient) {
@@ -14,8 +17,9 @@ export default class ReadyListener implements ListenerInterface {
 		for (const guild of guilds.values()) {
 			const g: Guild = await client.guilds.fetch(guild.id);
 
-			await g.members.fetch().catch((e) => client.logger.error(e));
+			await g.members.fetch().catch((error) => client.logger.error(error));
 		}
+
 		const guildSettings = await client.drizzle.select().from(guildsSettings);
 
 		const guildsInDb = guildSettings.map((guild) => guild.guildId);
@@ -23,11 +27,23 @@ export default class ReadyListener implements ListenerInterface {
 		const notInDbGuilds = client.guilds.cache
 			.filter((g) => !guildsInDb.includes(g.id))
 			.map((g) => {
-				return { guildId: g.id };
+				return { guildId: g.id, guildName: g.name, guildIcon: g.iconURL() };
 			});
 
-		if (notInDbGuilds.length < 0) return;
+		if (notInDbGuilds.length !== 0) {
+			await client.drizzle.insert(guildsSettings).values(notInDbGuilds);
+		}
 
-		await client.drizzle.insert(guildsSettings).values(notInDbGuilds);
+		await client.drizzle.transaction(async (trx) => {
+			for (const guild of guildSettings) {
+				if (!guild.guildName) {
+					const g: Guild = await client.guilds.fetch(guild.guildId);
+					await trx
+						.update(guildsSettings)
+						.set({ guildIcon: g.iconURL(), guildName: g.name })
+						.where(eq(guildsSettings.guildId, guild.guildId));
+				}
+			}
+		});
 	}
 }
