7ded41425e97cd1184ad79b5731c78ff1f746fdb
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-20 23:36:54 -0400

    feat(guildCreate): add guild name and icon setter

--- services/sleepymaid/src/listeners/client/guildCreate.ts
+++ services/sleepymaid/src/listeners/client/guildCreate.ts
@@ -1,13 +1,16 @@
+import { guildsSettings } from '@sleepymaid/db';
 import type { ListenerInterface } from '@sleepymaid/handler';
 import type { Guild } from 'discord.js';
 import type { SleepyMaidClient } from '../../lib/extensions/SleepyMaidClient';
-import { guildsSettings } from '@sleepymaid/db';
 
 export default class GuildCreateListener implements ListenerInterface {
 	public readonly name = 'guildCreate';
+
 	public readonly once = false;
 
 	public async execute(guild: Guild, client: SleepyMaidClient) {
-		return await client.drizzle.insert(guildsSettings).values({ guildId: guild.id });
+		return client.drizzle
+			.insert(guildsSettings)
+			.values({ guildId: guild.id, guildName: guild.name, guildIcon: guild.iconURL() });
 	}
 }
