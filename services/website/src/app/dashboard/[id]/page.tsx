0b7f10ba43e7b4344dfe53650f3198a6a34a94d5
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-20 23:39:06 -0400

    fix(page): remove old page comment

--- services/website/src/app/dashboard/[id]/page.tsx
+++ services/website/src/app/dashboard/[id]/page.tsx
@@ -16,22 +16,3 @@ const Page = async ({ params }: { readonly params: { id: string } }) => {
 };
 
 export default Page;
-
-/* export default async function Page({
-  params,
-}: {
-  readonly params: { id: string };
-}) {
-  const guildSettings = await api.guilds.getGuildSettings({
-    guildId: params.id,
-  });
-  const test = JSON.stringify(guildSettings);
-
-  return (
-    <div className="container flex items-center justify-center">
-      Guild id: {params.id}
-      <br />
-      {test}
-    </div>
-  );
-}*/
