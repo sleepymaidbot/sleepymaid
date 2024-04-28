e2f24ea7023bbdddaaa2e470cc34c17ee0b903e1
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-28 14:25:49 -0400

    feat(redirects): add invite redirects

--- services/website/next.config.js
+++ services/website/next.config.js
@@ -5,6 +5,30 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  transpilePackages: ["lucide-react"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/oauth2/authorize?client_id=613040835684073506&permissions=564583048014912&scope=bot",
+        permanent: true,
+      },
+      {
+        source: "/invite/:guildId",
+        destination:
+          "https://discord.com/oauth2/authorize?client_id=613040835684073506&permissions=564583048014912&scope=bot&guild_id=:guildId",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default config;
