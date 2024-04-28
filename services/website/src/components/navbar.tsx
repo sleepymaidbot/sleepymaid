cbceba7079892ffb5ed3844e2045024e5d8023eb
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-28 14:29:55 -0400

    refactor(navbar): remove navbar styles

--- services/website/src/components/navbar.tsx
+++ services/website/src/components/navbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ChevronDown, LogOut, Server } from "lucide-react";
 import Link from "next/link";
 import type { Session } from "next-auth";
@@ -13,11 +11,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
 
 export default function NavBar({
   session,
@@ -25,84 +18,63 @@ export default function NavBar({
   readonly session: Session | null;
 }) {
   return (
-    <div className="flex h-[5%] py-2 pl-5">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <Link
-            className="flex items-center justify-center align-middle"
-            href="/"
-          >
-            <Avatar className="mr-2">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/81372048?s=200&v=4" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className="align-baseline text-sm font-medium">
-              Sleepy Maid
-            </div>
-          </Link>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <div className="absolute right-0 mr-5 flex">
-        <NavigationMenu>
-          <NavigationMenuList>
-            {session ? (
-              <div className="flex">
-                <NavigationMenuItem className="mr-3">
-                  <ModeToggle />
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button variant="ghost">
-                        <Avatar>
-                          <AvatarImage
-                            src={
-                              session.user?.image ??
-                              "https://avatars.githubusercontent.com/u/81372048?s=200&v=4"
-                            }
-                          />
-                          <AvatarFallback>
-                            {session.user?.name
-                              ?.split(" ")
-                              .map((name) => name[0])}
-                          </AvatarFallback>
-                        </Avatar>
-                        <div className="ml-2 align-middle">
-                          {session.user?.name}
-                        </div>
-                        <ChevronDown />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuItem asChild>
-                        <Link className="flex" href="/dashboard" passHref>
-                          <Server className="pr-2" /> Servers
-                        </Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <Link className="flex" href="/api/auth/signout">
-                          <LogOut className="pr-2" />
-                          Sign out
-                        </Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </NavigationMenuItem>
-              </div>
-            ) : (
-              <div className="flex">
-                <NavigationMenuItem className="mr-3">
-                  <ModeToggle />
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Button variant="outline">
-                    <Link href="/api/auth/signin">Login</Link>
-                  </Button>
-                </NavigationMenuItem>
-              </div>
-            )}
-          </NavigationMenuList>
-        </NavigationMenu>
+    <div className="h-1/10 flex w-full items-center justify-between p-1">
+      <div>
+        <Link
+          className="flex items-center justify-center align-middle"
+          href="/"
+        >
+          <Avatar className="mr-2">
+            <AvatarImage src="https://github.com/sleepymaidbot.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="align-baseline text-sm font-medium">Sleepy Maid</div>
+        </Link>
+      </div>
+      <div className="flex flex-grow-0 items-center">
+        <ModeToggle />
+        {session ? (
+          <div className="ml-2 flex align-middle">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost">
+                  <Avatar>
+                    <AvatarImage
+                      src={
+                        session.user?.image ??
+                        "https://github.com/sleepymaidbot.png"
+                      }
+                    />
+                    <AvatarFallback>
+                      {session.user?.name?.split(" ").map((name) => name[0])}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="ml-2 align-middle">{session.user?.name}</div>
+                  <ChevronDown />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem asChild>
+                  <Link className="flex" href="/dashboard" passHref>
+                    <Server className="pr-2" /> Servers
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link className="flex" href="/api/auth/signout">
+                    <LogOut className="pr-2" />
+                    Sign out
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        ) : (
+          <div className="flex">
+            <Button className="ml-2" variant="outline">
+              <Link href="/api/auth/signin">Login</Link>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
