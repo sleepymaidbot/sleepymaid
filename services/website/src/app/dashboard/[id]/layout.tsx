4cda611f4019f8edb718eb893d2c12020ceb0f3f
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-28 14:28:08 -0400

    feat(dashboard): add dashboard layout

---
+++ services/website/src/app/dashboard/[id]/layout.tsx
@@ -0,0 +1,16 @@
+import LeftBar from "./_component/leftbar";
+
+export default function Layout({
+  children,
+  params,
+}: {
+  readonly children: React.ReactNode;
+  readonly params: { id: string };
+}) {
+  return (
+    <div className="flex h-full">
+      <LeftBar params={params} />
+      {children}
+    </div>
+  );
+}
