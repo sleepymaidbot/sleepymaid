32485e4710f7879ded764fabf2321b14ac56d83f
Ecorte <antoine.turgeon2005@hotmail.com>
2024-04-30 19:24:09 -0400

    fix(Loading): apply theme to loading

--- services/website/src/app/dashboard/loading.tsx
+++ services/website/src/app/dashboard/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
 	return (
 		<div className="flex h-screen items-center justify-center">
-			<div className="border-t-solid h-12 w-12 animate-spin rounded-full border-b-2 border-t-4 border-white" />
+			<div className="border-t-solid border-foreground h-12 w-12 animate-spin rounded-full border-b-2 border-t-4" />
 		</div>
 	);
 }
