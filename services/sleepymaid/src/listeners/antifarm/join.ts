62253f46f198289f910e0f8276b3598d568e8998
Ecorte <antoine.turgeon2005@hotmail.com>
2024-05-26 19:41:02 -0400

    feat(AntiFarm): add logging

--- services/sleepymaid/src/listeners/antifarm/join.ts
+++ services/sleepymaid/src/listeners/antifarm/join.ts
@@ -1,12 +1,13 @@
 import type { ListenerInterface } from "@sleepymaid/handler";
 import type { Guild } from "discord.js";
+import type { SleepyMaidClient } from "../../lib/extensions/SleepyMaidClient";
 
 export default class AntiFarmJoinListener implements ListenerInterface {
 	public readonly name = "guildCreate";
 
 	public readonly once = false;
 
-	public async execute(guild: Guild) {
+	public async execute(guild: Guild, client: SleepyMaidClient) {
 		await guild.members.fetch();
 		if (guild.members.cache.size < 50) {
 			return;
@@ -15,6 +16,9 @@ export default class AntiFarmJoinListener implements ListenerInterface {
 		const botCount = guild.members.cache.filter((member) => member.user.bot).size;
 		const nonBotCount = guild.members.cache.filter((member) => !member.user.bot).size;
 		if (botCount > nonBotCount) {
+			client.logger.info(
+				`${guild.name} has a bot count of ${botCount} and a non-bot count of ${nonBotCount}. This is a farm.`,
+			);
 			await guild.leave();
 		}
 	}
