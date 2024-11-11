8c5d437f83471945b837459957a09f203d4a48da
Ecorte <me@ecorte.xyz>
2024-11-11 13:04:20 -0500

    feat(helper): add leaderboard task

---
+++ services/helper/src/tasks/leaderboardroles.ts
@@ -0,0 +1,44 @@
+import { Context, Task } from "@sleepymaid/handler";
+import { HelperClient } from "../lib/extensions/HelperClient";
+import { userData } from "@sleepymaid/db";
+import { desc } from "drizzle-orm";
+
+const roles = {
+	0: "1305566376496205935",
+	1: "1305566419676827648",
+	2: "1305566538476290171",
+};
+
+const role = "1305566315603300382";
+
+export default class LeaderboardTask extends Task<HelperClient> {
+	public constructor(context: Context<HelperClient>) {
+		super(context, {
+			interval: "*/5 * * * *",
+			runOnStart: true,
+		});
+	}
+
+	public override async execute() {
+		const client = this.container.client;
+		client.logger.debug("Leaderboard roles task started");
+
+		const leaderboard = await client.drizzle.query.userData.findMany({
+			orderBy: desc(userData.currency),
+			limit: 3,
+		});
+
+		const guild = await client.guilds.fetch("862103656852619304");
+
+		const members = await guild.members.fetch();
+
+		for (const [_, member] of members) {
+			if (leaderboard.find((user) => user.userId === member.id)) {
+				const index = leaderboard.findIndex((user) => user.userId === member.id);
+				await member.roles.add([roles[index as keyof typeof roles], role], "Leaderboard roles update");
+			} else {
+				await member.roles.remove([role, ...Object.values(roles)], "Leaderboard roles update");
+			}
+		}
+	}
+}
