e9c2ac94924702913d3bd91a594f358a4a0add56
Ecorte <me@ecorte.xyz>
2024-11-14 19:48:45 -0500

    feat(Watcher): add ping command

---
+++ services/watcher/src/commands/chat/ping.ts
@@ -0,0 +1,47 @@
+import { Context, SlashCommand } from "@sleepymaid/handler";
+import { WatcherClient } from "../../lib/extensions/WatcherClient";
+import { APIEmbed, ChatInputCommandInteraction } from "discord.js";
+
+export default class extends SlashCommand<WatcherClient> {
+	constructor(context: Context<WatcherClient>) {
+		super(context, {
+			data: {
+				name: "ping",
+				description: "Ping the bot",
+			},
+		});
+	}
+
+	public override async execute(interaction: ChatInputCommandInteraction) {
+		const client = this.container.client;
+		const timestamp1 = interaction.createdTimestamp;
+		await interaction.reply("Pong!");
+		const timestamp2 = (await interaction.fetchReply()).createdTimestamp;
+		const botLatency = `\`\`\`\n ${Math.floor(timestamp2 - timestamp1)}ms \`\`\``;
+		const apiLatency = `\`\`\`\n ${Math.round(client.ws.ping)}ms \`\`\``;
+		const embed: APIEmbed = {
+			title: "Pong!  🏓",
+			fields: [
+				{
+					name: "Bot Latency",
+					value: botLatency,
+					inline: true,
+				},
+				{
+					name: "API Latency",
+					value: apiLatency,
+					inline: true,
+				},
+			],
+			footer: {
+				text: interaction.user.username,
+				icon_url: interaction.user.displayAvatarURL(),
+			},
+			timestamp: new Date(Date.now()).toISOString(),
+		};
+		await interaction.editReply({
+			content: null,
+			embeds: [embed],
+		});
+	}
+}
