0980e1ea06ba5fa6014d60e247c454a2e1746eb1
Ecorte <me@ecorte.xyz>
2024-11-20 14:25:15 -0500

    feat(sleepymaid): add selfroles command

---
+++ services/sleepymaid/src/commands/chat/global/selfrole.ts
@@ -0,0 +1,137 @@
+import type { Context } from "@sleepymaid/handler";
+import { SlashCommand } from "@sleepymaid/handler";
+import type { ApplicationCommandOptionChoiceData, ChatInputCommandInteraction } from "discord.js";
+import {
+	ApplicationCommandOptionType,
+	ApplicationCommandType,
+	ComponentType,
+	PermissionFlagsBits,
+	PermissionsBitField,
+} from "discord.js";
+import type { SleepyMaidClient } from "../../../lib/SleepyMaidClient";
+
+enum ActionType {
+	TOGGLE = "toggle",
+	ADD = "add",
+	REMOVE = "remove",
+}
+
+const options: Record<
+	ActionType,
+	ApplicationCommandOptionChoiceData<string> & { description: (roleId: string) => string; label: string }
+> = {
+	[ActionType.TOGGLE]: {
+		name: "Toggle",
+		value: "toggle",
+		description: (roleId) => "Click the button below to get or remove the <@&" + roleId + "> role!",
+		label: "Add/Remove the role",
+	},
+	[ActionType.ADD]: {
+		name: "Add",
+		value: "add",
+		description: (roleId) => "Click the button below to get the <@&" + roleId + "> role!",
+		label: "Add the role",
+	},
+	[ActionType.REMOVE]: {
+		name: "Remove",
+		value: "remove",
+		description: (roleId) => "Click the button below to remove the <@&" + roleId + "> role!",
+		label: "Remove the role",
+	},
+};
+
+export default class SelfRoleCommand extends SlashCommand<SleepyMaidClient> {
+	public constructor(context: Context<SleepyMaidClient>) {
+		super(context, {
+			data: {
+				name: "selfrole",
+				description: "[Admin only] Allow you to post a simple self role message.",
+				type: ApplicationCommandType.ChatInput,
+				defaultMemberPermissions: new PermissionsBitField([PermissionFlagsBits.ManageRoles]),
+				options: [
+					{
+						name: "role",
+						description: "The role to add/remove/toggle",
+						type: ApplicationCommandOptionType.Role,
+						required: true,
+					},
+					{
+						name: "action",
+						description: "What action to take with the role",
+						type: ApplicationCommandOptionType.String,
+						required: true,
+						choices: Object.values(options),
+					},
+				],
+			},
+		});
+	}
+
+	public override async execute(interaction: ChatInputCommandInteraction) {
+		if (!interaction.inCachedGuild()) return;
+		const client = this.container.client;
+		const role = interaction.options.getRole("role");
+		const action = interaction.options.getString("action");
+		if (!action || !Object.values(ActionType).includes(action as ActionType))
+			return this.container.logger.error("Invalid action");
+		if (!role) return this.container.logger.error("Invalid role");
+		if (
+			role.permissions.any([
+				PermissionFlagsBits.Administrator,
+				PermissionFlagsBits.BanMembers,
+				PermissionFlagsBits.KickMembers,
+				PermissionFlagsBits.ManageChannels,
+				PermissionFlagsBits.ManageGuildExpressions,
+				PermissionFlagsBits.ManageGuild,
+				PermissionFlagsBits.ManageMessages,
+				PermissionFlagsBits.ManageRoles,
+				PermissionFlagsBits.ManageWebhooks,
+			])
+		)
+			return interaction.reply({
+				content: "You cannot use this command on an admin role.",
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (!client.user) return;
+		if (!interaction.guild) return;
+		const bot = interaction.guild.members.cache.get(client.user.id);
+		if (!bot) return;
+		if (role.position >= bot.roles.highest.position)
+			return interaction.reply({
+				content: "You cannot use this command on a role higher than the bot.",
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (role.position >= interaction.member.roles.highest.position && interaction.user.id !== interaction.guild.ownerId)
+			return interaction.reply({
+				content: "You cannot use this command on a role higher than yours.",
+				ephemeral: true,
+				allowedMentions: { parse: [] },
+			});
+		if (!interaction.channel) return;
+		await interaction.reply({
+			content: "Self role message created!",
+			ephemeral: true,
+			allowedMentions: { parse: [] },
+		});
+		await interaction.channel.send({
+			content: options[action as ActionType].description(role.id),
+			allowedMentions: { parse: [] },
+			components: [
+				{
+					type: 1,
+					components: [
+						{
+							type: ComponentType.Button,
+							style: 1,
+							label: options[action as ActionType].label,
+							custom_id: "selfrole:" + role.id + ":" + action,
+						},
+					],
+				},
+			],
+		});
+		return null;
+	}
+}
---
+++ services/sleepymaid/src/listeners/misc/selfroles.ts
@@ -0,0 +1,47 @@
+import type { Context } from "@sleepymaid/handler";
+import { Listener } from "@sleepymaid/handler";
+import type { BaseInteraction } from "discord.js";
+import { SleepyMaidClient } from "../../lib/SleepyMaidClient";
+
+export default class SelfRoleListener extends Listener<"interactionCreate", SleepyMaidClient> {
+	public constructor(context: Context<SleepyMaidClient>) {
+		super(context, {
+			name: "interactionCreate",
+			once: false,
+		});
+	}
+
+	public override async execute(interaction: BaseInteraction) {
+		if (!interaction.inCachedGuild()) return;
+		if (!interaction.isButton()) return;
+		if (!interaction.customId.startsWith("selfrole:")) return;
+		await interaction.deferReply({ ephemeral: true });
+		const [_, roleId, action] = interaction.customId.split(":");
+		if (!roleId || !action) return interaction.editReply({ content: "Something went wrong." });
+		const role = interaction.guild?.roles.cache.get(roleId);
+		if (!role) return interaction.editReply({ content: "Something went wrong." });
+		if (!interaction.member) return interaction.editReply({ content: "Something went wrong." });
+		if (!interaction.channel) return interaction.editReply({ content: "Something went wrong." });
+		if (interaction.member.roles.cache.has(roleId)) {
+			if (action === "add") return interaction.editReply({ content: "You already have the role." });
+			await interaction.member.roles.remove(
+				roleId,
+				`Selfrole in #${interaction.channel.name} (${interaction.channel.id})`,
+			);
+			return interaction.editReply({
+				content: `You no longer have the role ${role.toString()}`,
+				allowedMentions: { parse: [] },
+			});
+		} else {
+			if (action === "remove") return interaction.editReply({ content: "You don't have the role yet." });
+			await interaction.member.roles.add(
+				roleId,
+				`Selfrole in #${interaction.channel.name} (${interaction.channel.id})`,
+			);
+			return interaction.editReply({
+				content: `You now have the role ${role.toString()}`,
+				allowedMentions: { parse: [] },
+			});
+		}
+	}
+}
