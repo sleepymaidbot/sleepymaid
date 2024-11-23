30fd3b707dd033803fd8d497846ecb5cb1ef5f67
Ecorte <me@ecorte.xyz>
2024-11-22 19:45:32 -0500

    refactor(DBCheck): rewrite dbcheck precondition

--- services/sleepymaid/src/preconditions/dbCheck.ts
+++ services/sleepymaid/src/preconditions/dbCheck.ts
@@ -1,72 +1,45 @@
 import { SleepyMaidClient } from "../lib/SleepyMaidClient";
 import { guildSettings, userData } from "@sleepymaid/db";
 import { Context, Precondition, type CommandInteractionTypeUnion } from "@sleepymaid/handler";
-import { eq } from "drizzle-orm";
-
 export default class DBCheckPrecondtion extends Precondition<SleepyMaidClient> {
 	public constructor(context: Context<SleepyMaidClient>) {
 		super(context);
 	}
 
 	public override async CommandRun(interaction: CommandInteractionTypeUnion) {
-		const client = this.container.client;
-		if (interaction.guild) {
-			const guild = interaction.guild;
-			const guildId = guild.id;
-			const guildSetting = await client.drizzle.query.guildSettings.findFirst({
-				where: eq(guildSettings.guildId, guildId),
-			});
-			if (!guildSetting) {
-				client.logger.info("No guild settings found, inserting...");
-				await this.container.client.drizzle.insert(guildSettings).values({
-					guildId: guild.id,
-					guildName: guild.name,
-					guildIcon: guild.iconURL(),
-				});
-			} else {
-				const updates: Partial<typeof guildSettings.$inferInsert> = {};
-				if (guildSetting.guildIcon !== (guild.iconURL() || "")) {
-					updates.guildIcon = guild.iconURL() || "";
-				}
-				if (guildSetting.guildName !== guild.name) {
-					updates.guildName = guild.name;
-				}
-				if (Object.keys(updates).length > 0) {
-					await this.container.client.drizzle
-						.update(guildSettings)
-						.set(updates)
-						.where(eq(guildSettings.guildId, guildId));
-				}
-			}
-		}
+		await this.container.drizzle.transaction(async (tx) => {
+			if (interaction.guild)
+				await tx
+					.insert(guildSettings)
+					.values({
+						guildId: interaction.guild.id,
+						guildName: interaction.guild.name,
+						guildIcon: interaction.guild.iconURL(),
+					})
+					.onConflictDoUpdate({
+						target: [guildSettings.guildId],
+						set: {
+							guildName: interaction.guild.name,
+							guildIcon: interaction.guild.iconURL(),
+						},
+					});
 
-		const user = interaction.user;
-		const userId = user.id;
-		const userDatas = await client.drizzle.query.userData.findFirst({
-			where: eq(userData.userId, userId),
+			await tx
+				.insert(userData)
+				.values({
+					userId: interaction.user.id,
+					userName: interaction.user.username,
+					displayName: interaction.user.displayName,
+					avatarHash: interaction.user.avatar,
+				})
+				.onConflictDoUpdate({
+					target: [userData.userId],
+					set: {
+						userName: interaction.user.username,
+						displayName: interaction.user.displayName,
+						avatarHash: interaction.user.avatar,
+					},
+				});
 		});
-		if (!userDatas) {
-			client.logger.info("No user data found, inserting...");
-			await this.container.client.drizzle.insert(userData).values({
-				userId: user.id,
-				userName: user.username,
-				displayName: user.displayName,
-				avatarHash: user.avatar,
-			});
-		} else {
-			const updates: Partial<typeof userData.$inferInsert> = {};
-			if (userDatas.userName !== user.username) {
-				updates.userName = user.username;
-			}
-			if (userDatas.avatarHash !== (user.avatar || "")) {
-				updates.avatarHash = user.avatar || "";
-			}
-			if (userDatas.displayName !== user.displayName) {
-				updates.displayName = user.displayName;
-			}
-			if (Object.keys(updates).length > 0) {
-				await this.container.client.drizzle.update(userData).set(updates).where(eq(userData.userId, userId));
-			}
-		}
 	}
 }
