ccd528ff2e0234bbf2eecc07edc4e6a1ddda9ac1
Ecorte <me@ecorte.xyz>
2024-11-23 22:23:59 -0500

    refactor(listeners): micro optimization to listeners

--- services/sleepymaid/src/listeners/client/guildCreate.ts
+++ services/sleepymaid/src/listeners/client/guildCreate.ts
@@ -17,6 +17,23 @@ export default class GuildCreateListener extends Listener<"guildCreate", SleepyM
 			`Joined guild ${guild.name} (${guild.id}) (${guild.memberCount} members) owned by ${guild.ownerId}`,
 		);
 
+		await guild.members.fetch();
+		if (guild.members.cache.size < 50) {
+			return;
+		}
+
+		const owner = await this.container.client.users.fetch(guild.ownerId);
+		if (owner.bot) return guild.leave();
+
+		const botCount = guild.members.cache.filter((member) => member.user.bot).size;
+		const nonBotCount = guild.members.cache.filter((member) => !member.user.bot).size;
+		if (botCount > nonBotCount) {
+			this.container.logger.info(
+				`${guild.name} has a bot count of ${botCount} and a non-bot count of ${nonBotCount}. This is a farm.`,
+			);
+			return await guild.leave();
+		}
+
 		return this.container.client.drizzle
 			.insert(guildSettings)
 			.values({ guildId: guild.id, guildName: guild.name, iconHash: guild.icon });
