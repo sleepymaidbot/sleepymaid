8ffec6661f0571d62a6f0670ae9d45bd8c3dcb3e
Ecorte <me@ecorte.xyz>
2024-11-24 20:54:29 -0500

    refactor(reminders): rewrite task

--- services/sleepymaid/src/tasks/reminders.ts
+++ services/sleepymaid/src/tasks/reminders.ts
@@ -2,24 +2,41 @@ import { Context, Task } from "@sleepymaid/handler";
 import { SleepyMaidClient } from "../lib/SleepyMaidClient";
 import { eq } from "drizzle-orm";
 import { lte } from "drizzle-orm";
-import { reminders } from "@sleepymaid/db";
+import { reminders, Reminder } from "@sleepymaid/db";
 
 export default class RemindersTask extends Task<SleepyMaidClient> {
+	private dbCooldown = 0;
+	private cachedReminders: Reminder[] = [];
+	private readonly CACHE_REFRESH_INTERVAL = 300_000; // 5 min
+
 	public constructor(context: Context<SleepyMaidClient>) {
 		super(context, {
-			interval: "*/5 * * * *",
+			interval: "* * * * *",
 			runOnStart: true,
 		});
+	}
+
+	private async refreshCache() {
+		if (Date.now() - this.dbCooldown < this.CACHE_REFRESH_INTERVAL) return;
+
+		this.container.logger.debug("Refreshing reminders cache");
+		this.cachedReminders = await this.container.drizzle.query.reminders.findMany({
+			where: lte(reminders.reminderTime, new Date(Date.now() + 360_000)), // 6 min
+		});
+		this.dbCooldown = Date.now();
 	}
 
 	public override async execute() {
 		this.container.logger.debug("Reminders task started");
 
-		const remindersList = await this.container.drizzle.query.reminders.findMany({
-			where: lte(reminders.reminderTime, new Date()),
-		});
+		await this.refreshCache();
 
-		for (const reminder of remindersList) {
+		const now = Date.now();
+		const dueReminders = this.cachedReminders.filter((reminder) => reminder.reminderTime.getTime() <= now - 1000);
+
+		if (dueReminders.length === 0) return;
+
+		for (const reminder of dueReminders) {
 			this.container.logger.info(`Processing reminder: ${reminder.reminderId}`);
 			const user = await this.container.client.users.fetch(reminder.userId);
 			if (!user) continue;
@@ -27,6 +44,7 @@ export default class RemindersTask extends Task<SleepyMaidClient> {
 			await user.send({ content: `Reminder: ${reminder.reminderName}` });
 
 			await this.container.drizzle.delete(reminders).where(eq(reminders.reminderId, reminder.reminderId));
+			this.cachedReminders = this.cachedReminders.filter((r) => r.reminderId !== reminder.reminderId);
 		}
 	}
 }
