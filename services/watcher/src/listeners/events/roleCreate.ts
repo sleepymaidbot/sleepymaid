196a70b978ab6b2fd292fd867f2b0af411b94fea
Ecorte <me@ecorte.xyz>
2024-11-26 16:33:54 -0500

    feat(Watcher): add roleCreate logs

---
+++ services/watcher/src/listeners/events/roleCreate.ts
@@ -0,0 +1,64 @@
+import { Context, Listener } from "@sleepymaid/handler";
+import { WatcherClient } from "../../lib/extensions/WatcherClient";
+import { Role, WebhookClient } from "discord.js";
+
+export default class extends Listener<"roleCreate", WatcherClient> {
+	constructor(context: Context<WatcherClient>) {
+		super(context, {
+			name: "roleCreate",
+			once: false,
+		});
+	}
+
+	public override async execute(role: Role) {
+		const channels = await this.container.manager.getLogChannel(role.guild.id);
+
+		if (!channels) return;
+
+		for (const channel of channels) {
+			if (!channel.roleEvents.create) continue;
+
+			const webhook = new WebhookClient({
+				id: channel.webhookId,
+				token: channel.webhookToken,
+			});
+
+			webhook.send({
+				username: `${this.container.client.user?.displayName}`,
+				avatarURL: this.container.client.user?.displayAvatarURL(),
+				threadId: channel.threadId ?? undefined,
+				embeds: [
+					{
+						title: "Role Created",
+						fields: [
+							{
+								name: "Role",
+								value: `${role.name} (${role.id})`,
+							},
+							{
+								name: "Color",
+								value: `${role.hexColor}`,
+							},
+							{
+								name: "Permissions",
+								value: `${role.permissions.toArray().join(", ")}`,
+							},
+							{
+								name: "Mentionable",
+								value: `${role.mentionable ? "Yes" : "No"}`,
+							},
+							{
+								name: "Hoisted",
+								value: `${role.hoist ? "Yes" : "No"}`,
+							},
+							{
+								name: "Position",
+								value: `${role.position}`,
+							},
+						],
+					},
+				],
+			});
+		}
+	}
+}
