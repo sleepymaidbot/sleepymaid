774bdd3c23c63d6bc0cdf40e3f280cce88b65d0e
Ecorte <me@ecorte.xyz>
2024-11-27 15:47:26 -0500

    feat(sleepymaid): add linked roles metadata

---
+++ services/sleepymaid/src/linkeddata.ts
@@ -0,0 +1,37 @@
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+import { ApplicationRoleConnectionMetadata } from "discord.js";
+
+(async () => {
+	const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
+
+	console.log("Updating metadata...");
+
+	const metaData: ApplicationRoleConnectionMetadata[] = [
+		{
+			type: 2,
+			key: "coins",
+			name: "Coins",
+			description: "The number of coins you have",
+			nameLocalizations: {
+				"en-US": "Coins",
+				fr: "Pièces",
+			},
+			descriptionLocalizations: {
+				"en-US": "The number of coins you have",
+				fr: "Le nombre de pièces que vous avez",
+			},
+		},
+	];
+
+	const data = await rest
+		.put(Routes.applicationRoleConnectionMetadata(process.env.DISCORD_CLIENT_ID!), {
+			body: metaData,
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+
+	console.log(data);
+	console.log("Done!");
+})();
