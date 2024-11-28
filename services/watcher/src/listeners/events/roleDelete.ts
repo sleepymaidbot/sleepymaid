c9455143cc24a02ecdd097779db58adba58f222d
Ecorte <me@ecorte.xyz>
2024-11-28 18:31:16 -0500

    feat(Watcher): add roleDelete

---
+++ services/watcher/src/listeners/events/roleDelete.ts
@@ -0,0 +1,61 @@
+import { Context, Listener } from "@sleepymaid/handler";
+import { WatcherClient } from "../../lib/extensions/WatcherClient";
+import { intToHexColor } from "@sleepymaid/util";
+import { Role } from "discord.js";
+
+export default class extends Listener<"roleDelete", WatcherClient> {
+	public constructor(context: Context<WatcherClient>) {
+		super(context, {
+			name: "roleDelete",
+			once: false,
+		});
+	}
+
+	public override async execute(role: Role) {
+		const channels = await this.container.manager.getLogChannel(role.guild.id);
+		if (!channels) return;
+
+		for (const channel of channels) {
+			if (!channel.roleEvents.delete) continue;
+
+			await this.container.manager.sendLog(channel, [
+				{
+					title: "Role Deleted",
+					color: role.color,
+					fields: [
+						{
+							name: "Role",
+							value: `${role.name} (${role.id})`,
+							inline: true,
+						},
+						{
+							name: "Color",
+							value: `${intToHexColor(role.color)}`,
+							inline: true,
+						},
+						{
+							name: "Hoisted",
+							value: `${role.hoist}`,
+							inline: true,
+						},
+						{
+							name: "Mentionable",
+							value: `${role.mentionable}`,
+							inline: true,
+						},
+						{
+							name: "Position",
+							value: `${role.position}`,
+							inline: true,
+						},
+						{
+							name: "Permissions",
+							value: `${role.permissions.toArray().join(", ")}`,
+							inline: true,
+						},
+					],
+				},
+			]);
+		}
+	}
+}
