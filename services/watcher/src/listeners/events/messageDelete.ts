2686991a7614e3e32df29a81aca883163f771fb9
Ecorte <me@ecorte.xyz>
2024-11-28 18:28:06 -0500

    refactor(messageDelete): run logic outside of loop

--- services/watcher/src/listeners/events/messageDelete.ts
+++ services/watcher/src/listeners/events/messageDelete.ts
@@ -15,44 +15,44 @@ export default class extends Listener<"messageDelete", WatcherClient> {
 		const channels = await this.container.manager.getLogChannel(message.guild.id);
 		if (!channels) return;
 
+		const content = message.content ? `\`\`\`${message.content}\`\`\`` : "No content";
+
+		const fields: APIEmbedField[] = [
+			{
+				name: "Author",
+				value: `${message.author.displayName} (${message.author.id})`,
+				inline: true,
+			},
+			{
+				name: "Channel",
+				value: `${message.channel}`,
+				inline: true,
+			},
+			{
+				name: "Content",
+				value: content,
+				inline: false,
+			},
+		];
+
+		if (message.attachments.size > 0) {
+			fields.push({
+				name: "Attachments",
+				value: message.attachments.map((a) => a.url).join("\n"),
+				inline: false,
+			});
+		}
+
+		if (message.stickers.size > 0) {
+			fields.push({
+				name: "Stickers",
+				value: message.stickers.map((s) => s.name).join("\n"),
+				inline: false,
+			});
+		}
+
 		for (const channel of channels) {
 			if (!channel.messageEvents.delete) continue;
-
-			const content = message.content ? `\`\`\`${message.content}\`\`\`` : "No content";
-
-			const fields: APIEmbedField[] = [
-				{
-					name: "Author",
-					value: `${message.author.displayName} (${message.author.id})`,
-					inline: true,
-				},
-				{
-					name: "Channel",
-					value: `${message.channel}`,
-					inline: true,
-				},
-				{
-					name: "Content",
-					value: content,
-					inline: false,
-				},
-			];
-
-			if (message.attachments.size > 0) {
-				fields.push({
-					name: "Attachments",
-					value: message.attachments.map((a) => a.url).join("\n"),
-					inline: false,
-				});
-			}
-
-			if (message.stickers.size > 0) {
-				fields.push({
-					name: "Stickers",
-					value: message.stickers.map((s) => s.name).join("\n"),
-					inline: false,
-				});
-			}
 
 			await this.container.manager.sendLog(channel, [
 				{
