dd1de1128ea93b42f27b3c2d847b757378c2fcac
Ecorte <me@ecorte.xyz>
2024-12-13 12:36:38 -0500

    feat(Watcher): add member join and leave

---
+++ services/watcher/src/listeners/events/guildMemberRemove.ts
@@ -0,0 +1,37 @@
+import { Context, Listener } from "@sleepymaid/handler";
+import { WatcherClient } from "../../lib/extensions/WatcherClient";
+import { GuildMember, Colors, APIEmbed } from "discord.js";
+
+export default class extends Listener<"guildMemberRemove", WatcherClient> {
+	public constructor(context: Context<WatcherClient>) {
+		super(context, {
+			name: "guildMemberRemove",
+			once: false,
+		});
+	}
+
+	public override async execute(member: GuildMember) {
+		const channels = (await this.container.manager.getLogChannel(member.guild.id))?.filter((c) => c.memberEvents.join);
+		if (!channels || channels.length === 0) return;
+
+		const embed: APIEmbed = {
+			title: "Member Left",
+			color: Colors.Red,
+			thumbnail: {
+				url: member.user.displayAvatarURL(),
+			},
+			fields: [
+				{
+					name: "User",
+					value: `${member.displayName} (${member.id})`,
+					inline: true,
+				},
+			],
+			timestamp: new Date().toISOString(),
+		};
+
+		for (const channel of channels) {
+			await this.container.manager.sendLog(channel, { embeds: [embed] });
+		}
+	}
+}
