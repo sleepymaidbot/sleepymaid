78d47bb43a75926b3301fd4b00e0e0c27cd74193
Ecorte <me@ecorte.xyz>
2025-04-07 15:16:58 -0400

    fix(shared): add back channelNames

--- packages/shared/src/misc.ts
+++ packages/shared/src/misc.ts
@@ -1,3 +1,16 @@
+import { ChannelType } from "discord.js";
+
 export function formatNumber(number: number): string {
 	return new Intl.NumberFormat("en-US", { useGrouping: true }).format(number);
 }
+
+export const channelNames: Partial<Record<ChannelType, string>> = {
+	[ChannelType.GuildText]: "Text Channel",
+	[ChannelType.GuildVoice]: "Voice Channel",
+	[ChannelType.GuildCategory]: "Category",
+	[ChannelType.GuildStageVoice]: "Stage Channel",
+	[ChannelType.GuildForum]: "Forum Channel",
+	[ChannelType.GuildAnnouncement]: "Announcement Channel",
+	[ChannelType.GuildDirectory]: "Directory Channel",
+	[ChannelType.GuildMedia]: "Media Channel",
+};
