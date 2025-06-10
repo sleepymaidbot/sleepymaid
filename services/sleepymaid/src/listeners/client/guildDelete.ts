cf84d61815cc24ca80505d8d882bf93aaca7ae15
Ecorte <me@ecorte.xyz>
2025-06-10 16:27:25 -0400

    feat(listeners): add guildDelete listener

---
+++ services/sleepymaid/src/listeners/client/guildDelete.ts
@@ -0,0 +1,17 @@
+import type { Context } from "@sleepymaid/handler";
+import { Listener } from "@sleepymaid/handler";
+import type { Guild } from "discord.js";
+import type { SleepyMaidClient } from "../../lib/SleepyMaidClient";
+
+export default class extends Listener<"guildDelete", SleepyMaidClient> {
+	public constructor(context: Context<SleepyMaidClient>) {
+		super(context, {
+			name: "guildDelete",
+			once: false,
+		});
+	}
+
+	public override async execute(guild: Guild) {
+		this.container.client.logger.info(`Left guild ${guild.name} (${guild.id})`);
+	}
+}
