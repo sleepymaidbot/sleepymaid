eb06e21f8896b9212cf15ad84bea16695805142f
Ecorte <me@ecorte.xyz>
2025-10-15 12:34:54 -0400

    fix(mamayo): restrict to one server

--- services/helper/src/listeners/mamayo/amine.ts
+++ services/helper/src/listeners/mamayo/amine.ts
@@ -13,7 +13,7 @@ export default class extends Listener<"messageCreate", HelperClient> {
 	public override async execute(message: Message) {
 		if (!message.channel.isSendable()) return;
 		if (!message.guild) return;
-		//if (message.guild.id !== "1150780245151068332") return;
+		if (message.guild.id !== "1150780245151068332") return;
 
 		if (message.content.includes("amine")) {
 			await message.react("<:amine:1427832531230789712>");
