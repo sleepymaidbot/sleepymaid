a35ee594b623a276f24363e95df4d850239e9b3c
Ecorte <me@ecorte.xyz>
2026-02-02 17:09:35 -0500

    feat(helper): add basic alpha network tracking

---
+++ services/helper/src/tasks/alpha.ts
@@ -0,0 +1,42 @@
+import { type Context, Task } from "@sleepymaid/handler"
+import { GameDig } from "gamedig"
+import type { HelperClient } from "../lib/extensions/HelperClient"
+
+export default class extends Task<HelperClient> {
+	public constructor(context: Context<HelperClient>) {
+		super(context, {
+			interval: "*/5 * * * *",
+			runOnStart: true,
+		})
+	}
+
+	public override async execute() {
+		const client = this.container.client
+		client.logger.debug("Alpha network task started")
+
+		GameDig.query({
+			type: "minecraft",
+			host: "alpha.hypixel.net",
+			port: 25565,
+		})
+			.then((result) => {
+				const maxplayers = result.maxplayers
+				const numplayers = result.numplayers
+
+				client.logger.debug(`Max players: ${maxplayers}, Num players: ${numplayers}`)
+
+				const guild = client.guilds.cache.get("821717486217986098")
+				if (!guild) return
+
+				const channel = guild.channels.cache.get("1467994738199363727")
+				if (!channel || !channel.isSendable()) return
+
+				if (maxplayers === 0) return
+
+				channel.send(`**Alpha Network**\nMax players: ${maxplayers}, Num players: ${numplayers}`)
+			})
+			.catch((error) => {
+				client.logger.error(error)
+			})
+	}
+}
