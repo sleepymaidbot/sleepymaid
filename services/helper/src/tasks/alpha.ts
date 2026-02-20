1275199d3c57c6a94d1ff334b405eb6fbadebccb
Ecorte <me@ecorte.xyz>
2026-02-20 11:29:38 -0500

    feat(Handler): add deferType

--- services/helper/src/tasks/alpha.ts
+++ services/helper/src/tasks/alpha.ts
@@ -1,49 +1,45 @@
-import { type Context, Task } from '@sleepymaid/handler';
-import { GameDig } from 'gamedig';
-import type { HelperClient } from '../lib/extensions/HelperClient';
+import { type Context, Task } from "@sleepymaid/handler"
+import { GameDig } from "gamedig"
+import type { HelperClient } from "../lib/extensions/HelperClient"
 
-const enabled = false;
+const enabled = false
 
 export default class extends Task<HelperClient> {
-  public constructor(context: Context<HelperClient>) {
-    super(context, {
-      interval: '*/5 * * * *',
-      runOnStart: true,
-    });
-  }
+	public constructor(context: Context<HelperClient>) {
+		super(context, {
+			interval: "*/5 * * * *",
+			runOnStart: true,
+		})
+	}
 
-  public override async execute() {
-    if (!enabled) return;
-    const client = this.container.client;
-    client.logger.debug('Alpha network task started');
+	public override async execute() {
+		if (!enabled) return
+		const client = this.container.client
+		client.logger.debug("Alpha network task started")
 
-    GameDig.query({
-      type: 'minecraft',
-      host: 'alpha.hypixel.net',
-      port: 25565,
-    })
-      .then((result) => {
-        const maxplayers = result.maxplayers;
-        const numplayers = result.numplayers;
+		GameDig.query({
+			type: "minecraft",
+			host: "alpha.hypixel.net",
+			port: 25565,
+		})
+			.then((result) => {
+				const maxplayers = result.maxplayers
+				const numplayers = result.numplayers
 
-        client.logger.debug(
-          `Max players: ${maxplayers}, Num players: ${numplayers}`,
-        );
+				client.logger.debug(`Max players: ${maxplayers}, Num players: ${numplayers}`)
 
-        const guild = client.guilds.cache.get('821717486217986098');
-        if (!guild) return;
+				const guild = client.guilds.cache.get("821717486217986098")
+				if (!guild) return
 
-        const channel = guild.channels.cache.get('1467994738199363727');
-        if (!channel || !channel.isSendable()) return;
+				const channel = guild.channels.cache.get("1467994738199363727")
+				if (!channel || !channel.isSendable()) return
 
-        if (maxplayers === 0) return;
+				if (maxplayers === 0) return
 
-        channel.send(
-          `**Alpha Network**\nMax players: ${maxplayers}, Num players: ${numplayers}`,
-        );
-      })
-      .catch(() => {
-        return;
-      });
-  }
+				channel.send(`**Alpha Network**\nMax players: ${maxplayers}, Num players: ${numplayers}`)
+			})
+			.catch(() => {
+				return
+			})
+	}
 }
